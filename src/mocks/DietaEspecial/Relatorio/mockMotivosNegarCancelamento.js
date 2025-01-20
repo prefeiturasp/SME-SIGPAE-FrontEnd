2c0fef52398068c317c67a9d16aec5fc0c70eba0
Calvin Rossinhole <calvin.masters@gmail.com>
2025-01-20 15:24:04 -0300

    test(124122): :white_check_mark: implementa teste unitario relatorio pendente dieta especial

---
+++ src/mocks/DietaEspecial/Relatorio/mockMotivosNegarCancelamento.js
@@ -0,0 +1,37 @@
+export const mockMotivosNegarCancelamento = [
+  {
+    id: 1,
+    descricao: "Dúvidas/ Orientações diversas",
+    processo: "CANCELAMENTO",
+  },
+  {
+    id: 2,
+    descricao: "Laudo de alta não confere com a dieta autorizada",
+    processo: "CANCELAMENTO",
+  },
+  {
+    id: 3,
+    descricao: "Prescritor não médico/ nutricionista/ fonoaudiólogo",
+    processo: "CANCELAMENTO",
+  },
+  {
+    id: 4,
+    descricao: "Outro documento que não seja laudo (exames)",
+    processo: "CANCELAMENTO",
+  },
+  {
+    id: 5,
+    descricao: "Laudo sem Nome do Aluno",
+    processo: "CANCELAMENTO",
+  },
+  {
+    id: 6,
+    descricao: "Divergência no nome do aluno: Laudo X sistema",
+    processo: "CANCELAMENTO",
+  },
+  {
+    id: 7,
+    descricao: "Detalhamento/ Esclarecimentos da Orientação Nutricional",
+    processo: "CANCELAMENTO",
+  },
+];
