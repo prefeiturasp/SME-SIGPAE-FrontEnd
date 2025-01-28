c5e0e2dbb33fddf04d8144b8983d08beabb50f65
Luis Henrique Zimmermann <lhzimmermann@gmail.com>
2025-01-28 16:35:53 -0300

    feat(124110): alteração de campos na ficha tecnica

--- src/interfaces/pre_recebimento.interface.ts
+++ src/interfaces/pre_recebimento.interface.ts
@@ -202,9 +202,9 @@ export interface FichaTecnicaDetalhada {
   gluten: boolean;
   lactose: boolean;
   lactose_detalhe: string;
-  porcao: number;
+  porcao: string;
   unidade_medida_porcao: UnidadeMedidaSimples;
-  valor_unidade_caseira: number;
+  valor_unidade_caseira: string;
   unidade_medida_caseira: string;
   informacoes_nutricionais: InformacoesNutricionaisFichaTecnica[];
   prazo_validade_descongelamento: string;
