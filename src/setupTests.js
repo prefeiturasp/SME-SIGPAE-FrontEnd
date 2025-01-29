c73912bd84be16e402eaa13a1d8ec0e353674d5f
Calvin Rossinhole <calvin.masters@gmail.com>
2025-01-29 18:58:44 -0300

    feat(-): :sparkles: corrige bug teste usando antd

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,3 +4,21 @@ jestPreviewConfigure({
   // Opt-in to automatic mode to preview failed test case automatically.
   autoPreview: true,
 });
+
+const { querySelector, matches } = window.Element.prototype;
+
+window.Element.prototype.querySelector = function (selector) {
+  try {
+    return querySelector.call(this, selector);
+  } catch (e) {
+    return null;
+  }
+};
+
+window.Element.prototype.matches = function (selector) {
+  try {
+    return matches.call(this, selector);
+  } catch (e) {
+    return false;
+  }
+};
