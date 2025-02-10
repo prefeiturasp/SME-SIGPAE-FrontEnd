4931387ac60809298255968ed8456ba88e81daac
Calvin Rossinhole <calvin.masters@gmail.com>
2025-02-10 16:24:35 -0300

    refactor(124120): :hammer: todos os cards renderizam

--- src/components/screens/DashboardGestaoProduto/index.jsx
+++ src/components/screens/DashboardGestaoProduto/index.jsx
@@ -3,19 +3,38 @@ import { InputText } from "components/Shareable/Input/InputText";
 import { ASelect } from "components/Shareable/MakeField";
 import { GESTAO_PRODUTO } from "configs/constants";
 import { listarCardsPermitidos } from "helpers/gestaoDeProdutos";
-import { dataAtual } from "helpers/utilities";
+import { dataAtual, usuarioEhEmpresa } from "helpers/utilities";
 import HTTP_STATUS from "http-status-codes";
 import React, { useEffect, useState } from "react";
 import { Field, Form } from "react-final-form";
-import { getProdutosPendenteHomologacao } from "services/dashboardGestaoProduto";
+import {
+  getProdutosAguardandoAmostraAnaliseSensorial,
+  getProdutosAguardandoAnaliseReclamacao,
+  getProdutosCorrecaoDeProdutos,
+  getProdutosHomologados,
+  getProdutosNaoHomologados,
+  getProdutosPendenteHomologacao,
+  getProdutosQuestionamentoDaCODAE,
+  getProdutosSuspensos,
+} from "services/dashboardGestaoProduto";
 import { getNomesUnicosEditais } from "services/produto.service";
 import { CardPainel } from "./componentes/CardPainel";
 import { formataCards } from "./helper";
-import { usuarioEhEmpresa } from "../../../helpers/utilities";
 
 export const DashboardGestaoProduto = () => {
   const [editais, setEditais] = useState();
   const [pendenteHomologacao, setPendenteHomologacao] = useState();
+  const [suspensos, setSuspensos] = useState();
+  const [homologados, setHomologados] = useState();
+  const [naoHomologados, setNaoHomologados] = useState();
+  const [aguardandoAnaliseReclamacoes, setAguardandoAnaliseReclamacoes] =
+    useState();
+  const [correcaoDeProdutos, setCorrecaoDeProdutos] = useState();
+  const [
+    aguardandoAmostraAnaliseSensorial,
+    setAguardandoAmostraAnaliseSensorial,
+  ] = useState();
+  const [questionamentoDaCODAE, setQuestionamentoDaCODAE] = useState();
 
   const [cards] = useState(listarCardsPermitidos());
 
@@ -46,8 +65,92 @@ export const DashboardGestaoProduto = () => {
     }
   };
 
+  const getProdutosSuspensosAsync = async () => {
+    const response = await getProdutosSuspensos();
+    if (response.status === HTTP_STATUS.OK) {
+      setSuspensos(response.data.results);
+    } else {
+      setErro(
+        "Erro ao carregar produtos suspensos. Tente novamente mais tarde."
+      );
+    }
+  };
+
+  const getProdutosHomologadosAsync = async () => {
+    const response = await getProdutosHomologados();
+    if (response.status === HTTP_STATUS.OK) {
+      setHomologados(response.data.results);
+    } else {
+      setErro(
+        "Erro ao carregar produtos homologados. Tente novamente mais tarde."
+      );
+    }
+  };
+
+  const getProdutosNaoHomologadosAsync = async () => {
+    const response = await getProdutosNaoHomologados();
+    if (response.status === HTTP_STATUS.OK) {
+      setNaoHomologados(response.data.results);
+    } else {
+      setErro(
+        "Erro ao carregar produtos não homologados. Tente novamente mais tarde."
+      );
+    }
+  };
+
+  const getProdutosAguardandoAnaliseReclamacaoAsync = async () => {
+    const response = await getProdutosAguardandoAnaliseReclamacao();
+    if (response.status === HTTP_STATUS.OK) {
+      setAguardandoAnaliseReclamacoes(response.data.results);
+    } else {
+      setErro(
+        "Erro ao carregar produtos aguardando análise reclamação. Tente novamente mais tarde."
+      );
+    }
+  };
+
+  const getProdutosCorrecaoDeProdutosAsync = async () => {
+    const response = await getProdutosCorrecaoDeProdutos();
+    if (response.status === HTTP_STATUS.OK) {
+      setCorrecaoDeProdutos(response.data.results);
+    } else {
+      setErro(
+        "Erro ao carregar produtos em correção de produtos. Tente novamente mais tarde."
+      );
+    }
+  };
+
+  const getProdutosAguardandoAmostraAnaliseSensorialAsync = async () => {
+    const response = await getProdutosAguardandoAmostraAnaliseSensorial();
+    if (response.status === HTTP_STATUS.OK) {
+      setAguardandoAmostraAnaliseSensorial(response.data.results);
+    } else {
+      setErro(
+        "Erro ao carregar produtos em aguardando amostra analise sensorial. Tente novamente mais tarde."
+      );
+    }
+  };
+
+  const getProdutosQuestionamentoDaCODAEAsync = async () => {
+    const response = await getProdutosQuestionamentoDaCODAE();
+    if (response.status === HTTP_STATUS.OK) {
+      setQuestionamentoDaCODAE(response.data.results);
+    } else {
+      setErro(
+        "Erro ao carregar produtos em questionamento da CODAE. Tente novamente mais tarde."
+      );
+    }
+  };
+
   const getProdutosAsync = async () => {
     await getProdutosPendenteHomologacaoAsync();
+    await getProdutosSuspensosAsync();
+    await getProdutosHomologadosAsync();
+    await getProdutosNaoHomologadosAsync();
+    await getProdutosAguardandoAnaliseReclamacaoAsync();
+    await getProdutosCorrecaoDeProdutosAsync();
+    await getProdutosAguardandoAmostraAnaliseSensorialAsync();
+    await getProdutosQuestionamentoDaCODAEAsync();
   };
 
   const apontaParaFormularioDeAlteracao = (titulo) => {
@@ -64,13 +167,34 @@ export const DashboardGestaoProduto = () => {
     }
   };
 
+  const getProdutosPorCard = {
+    "Produtos suspensos": suspensos,
+    Homologados: homologados,
+    "Não homologados": naoHomologados,
+    "Aguardando análise das reclamações": aguardandoAnaliseReclamacoes,
+    "Correções de Produtos": correcaoDeProdutos,
+    "Aguardando amostra para análise sensorial":
+      aguardandoAmostraAnaliseSensorial,
+    "Pendentes de homologação": pendenteHomologacao,
+    "Responder Questionamentos da CODAE": questionamentoDaCODAE,
+  };
+
   useEffect(() => {
     Promise.all([getEditaisAsync(), getProdutosAsync()]).then(() => {
       setLoading(false);
     });
   }, []);
 
-  const LOADING_INICIAL = !editais && !pendenteHomologacao;
+  const LOADING_INICIAL =
+    !editais ||
+    !pendenteHomologacao ||
+    !suspensos ||
+    !homologados ||
+    !naoHomologados ||
+    !aguardandoAnaliseReclamacoes ||
+    !correcaoDeProdutos ||
+    !aguardandoAmostraAnaliseSensorial ||
+    !questionamentoDaCODAE;
 
   return (
     <div className="card mt-3">
@@ -93,23 +217,21 @@ export const DashboardGestaoProduto = () => {
                         </div>
                       </div>
                       <div className="row">
-                        {editais && (
-                          <div className="col-4 produtos-edital">
-                            <Field
-                              component={ASelect}
-                              showSearch
-                              name="edital"
-                              placeholder="Número do Edital"
-                              disabled={LOADING_INICIAL || loading}
-                              options={[
-                                { label: "Número do Edital", value: "" },
-                              ].concat(editais)}
-                              onChange={(value) => {
-                                form.change("edital", value);
-                              }}
-                            />
-                          </div>
-                        )}
+                        <div className="col-4 produtos-edital">
+                          <Field
+                            component={ASelect}
+                            showSearch
+                            name="edital"
+                            placeholder="Número do Edital"
+                            disabled={LOADING_INICIAL || loading}
+                            options={[
+                              { label: "Número do Edital", value: "" },
+                            ].concat(editais)}
+                            onChange={(value) => {
+                              form.change("edital", value);
+                            }}
+                          />
+                        </div>
                         <div className="col-4">
                           <Field
                             component={InputText}
@@ -142,27 +264,24 @@ export const DashboardGestaoProduto = () => {
                         </div>
                       </div>
                       <div className="row">
-                        {pendenteHomologacao &&
-                          cards.map((card, index) => {
-                            return (
-                              <div key={index} className="col-6 mt-3">
-                                <CardPainel
-                                  cardTitle={card.titulo}
-                                  cardType={card.style}
-                                  produtos={formataCards(
-                                    pendenteHomologacao,
-                                    apontaParaFormularioDeAlteracao(
-                                      card.titulo
-                                    ),
-                                    card.titulo
-                                  )}
-                                  icon={card.icon}
-                                  href={`/${GESTAO_PRODUTO}/${card.rota}`}
-                                  hrefCard={card.href_card}
-                                />
-                              </div>
-                            );
-                          })}
+                        {cards.map((card, index) => {
+                          return (
+                            <div key={index} className="col-6 mt-3">
+                              <CardPainel
+                                cardTitle={card.titulo}
+                                cardType={card.style}
+                                produtos={formataCards(
+                                  getProdutosPorCard[card.titulo],
+                                  apontaParaFormularioDeAlteracao(card.titulo),
+                                  card.titulo
+                                )}
+                                icon={card.icon}
+                                href={`/${GESTAO_PRODUTO}/${card.rota}`}
+                                hrefCard={card.href_card}
+                              />
+                            </div>
+                          );
+                        })}
                       </div>
                     </form>
                   )}
--- src/services/dashboardGestaoProduto/index.jsx
+++ src/services/dashboardGestaoProduto/index.jsx
@@ -9,3 +9,66 @@ export const getProdutosPendenteHomologacao = async (params) => {
     return data;
   }
 };
+
+export const getProdutosSuspensos = async (params) => {
+  const url = `/dashboard-produtos/suspensos/`;
+  const response = await axios.get(url, { params }).catch(ErrorHandlerFunction);
+  if (response) {
+    const data = { data: response.data, status: response.status };
+    return data;
+  }
+};
+
+export const getProdutosHomologados = async (params) => {
+  const url = `/dashboard-produtos/homologados/`;
+  const response = await axios.get(url, { params }).catch(ErrorHandlerFunction);
+  if (response) {
+    const data = { data: response.data, status: response.status };
+    return data;
+  }
+};
+
+export const getProdutosNaoHomologados = async (params) => {
+  const url = `/dashboard-produtos/nao-homologados/`;
+  const response = await axios.get(url, { params }).catch(ErrorHandlerFunction);
+  if (response) {
+    const data = { data: response.data, status: response.status };
+    return data;
+  }
+};
+
+export const getProdutosAguardandoAnaliseReclamacao = async (params) => {
+  const url = `/dashboard-produtos/aguardando-analise-reclamacao/`;
+  const response = await axios.get(url, { params }).catch(ErrorHandlerFunction);
+  if (response) {
+    const data = { data: response.data, status: response.status };
+    return data;
+  }
+};
+
+export const getProdutosCorrecaoDeProdutos = async (params) => {
+  const url = `/dashboard-produtos/correcao-de-produtos/`;
+  const response = await axios.get(url, { params }).catch(ErrorHandlerFunction);
+  if (response) {
+    const data = { data: response.data, status: response.status };
+    return data;
+  }
+};
+
+export const getProdutosAguardandoAmostraAnaliseSensorial = async (params) => {
+  const url = `/dashboard-produtos/aguardando-amostra-analise-sensorial/`;
+  const response = await axios.get(url, { params }).catch(ErrorHandlerFunction);
+  if (response) {
+    const data = { data: response.data, status: response.status };
+    return data;
+  }
+};
+
+export const getProdutosQuestionamentoDaCODAE = async (params) => {
+  const url = `/dashboard-produtos/questionamento-da-codae/`;
+  const response = await axios.get(url, { params }).catch(ErrorHandlerFunction);
+  if (response) {
+    const data = { data: response.data, status: response.status };
+    return data;
+  }
+};
