d4e944bc03391c11f8c5d345665a739fcc3dcd2b
Murilo Ferri Schirmer <murilo.schirmer@spassu.com.br>
2025-02-26 16:42:24 -0300

    Testes unitários para a ReclamacaoProduto e componentes associados. Foco no campo edital adicionado na história

---
+++ src/mocks/services/produto.service/mockGetNovaReclamacaoNomesProdutos.js
@@ -0,0 +1,60 @@
+export const mockGetNovaReclamacaoNomesProdutos = {
+  results: [
+    {
+      uuid: "uuid",
+      nome: "ABACATE",
+    },
+    {
+      uuid: "uuid",
+      nome: "AD VITAE ILLUM POSSIMUS SIT ANIMI DOLOR SAPIENTE INVENTORE VELIT PRAESENTIUM ET COMMODI RATIONE A",
+    },
+    {
+      uuid: "uuid",
+      nome: "ANIMAL PERFORMANCE GLASS",
+    },
+    {
+      uuid: "uuid",
+      nome: "ARROZ INTEGRAL TIPO 3",
+    },
+    {
+      uuid: "uuid",
+      nome: "BODY DEBATE MOST",
+    },
+    {
+      uuid: "uuid",
+      nome: "CHÁ MATTE",
+    },
+    {
+      uuid: "uuid",
+      nome: "COXA DE FRANGO",
+    },
+    {
+      uuid: "uuid",
+      nome: "FORMIGA",
+    },
+    {
+      uuid: "uuid",
+      nome: "GELATINA AMORA",
+    },
+    {
+      uuid: "uuid",
+      nome: "LAMEN",
+    },
+    {
+      uuid: "uuid",
+      nome: "MILHARINA",
+    },
+    {
+      uuid: "uuid",
+      nome: "MOLESTIAS DOLOR REICIENDIS PARIATUR DOLORUM",
+    },
+    {
+      uuid: "uuid",
+      nome: "SUCO UVA",
+    },
+    {
+      uuid: "uuid",
+      nome: "YOGURTE DIET",
+    },
+  ],
+};
