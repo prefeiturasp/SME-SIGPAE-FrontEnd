17d61cae28d4d7316463956dfc129b066acdfca3
Calvin Rossinhole <calvin.masters@gmail.com>
2025-03-12 17:50:42 -0300

    refactor(126318): :hammer: refatora formulario de alteracao de cardapio pt7

---
+++ src/components/AlteracaoDeCardapio/Escola/components/Rascunhos/index.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+export const Rascunhos = ({ ...props }) => {
+  const { rascunhos, removerRascunho, carregarRascunho, form } = props;
+
+  return rascunhos.map((alteracaoDeCardapio, key) => {
+    const { uuid, id_externo } = alteracaoDeCardapio;
+    let backgroundColor =
+      alteracaoDeCardapio.status === "SALVO" ? "#82B7E8" : "#DADADA";
+    return (
+      <div key={key} className="bg-white border rounded mt-3">
+        <div className="mt-2">
+          <label className="bold ms-3">
+            {`Alteração do Tipo de Alimentação # ${id_externo}`}
+          </label>
+          <span
+            className="ms-3 p-1 border rounded"
+            style={{ background: backgroundColor }}
+          >
+            {alteracaoDeCardapio.status}
+          </span>
+        </div>
+        <div className="icon-draft-card float-end">
+          Salvo em: {alteracaoDeCardapio.criado_em}
+          <span onClick={() => removerRascunho(id_externo, uuid, form)}>
+            <i className="fas fa-trash" />
+          </span>
+          <span onClick={() => carregarRascunho(alteracaoDeCardapio, form)}>
+            <i className="fas fa-edit" />
+          </span>
+        </div>
+        <div className="ms-3">
+          <p>
+            {alteracaoDeCardapio.data
+              ? `Dia: ${alteracaoDeCardapio.data}`
+              : alteracaoDeCardapio.data_inicial ===
+                alteracaoDeCardapio.data_final
+              ? `Dia: ${
+                  alteracaoDeCardapio.data_inicial ||
+                  alteracaoDeCardapio.alterar_dia
+                }`
+              : `De ${alteracaoDeCardapio.data_inicial} a ${alteracaoDeCardapio.data_final}`}
+          </p>
+        </div>
+      </div>
+    );
+  });
+};
--- src/components/AlteracaoDeCardapio/Escola/index.jsx
+++ src/components/AlteracaoDeCardapio/Escola/index.jsx
@@ -6,10 +6,7 @@ import {
 } from "components/Shareable/Botao/constants";
 import CardMatriculados from "components/Shareable/CardMatriculados";
 import CKEditorField from "components/Shareable/CKEditorField";
-import { InputComData } from "components/Shareable/DatePicker";
-import { InputText } from "components/Shareable/Input/InputText";
 import ModalDataPrioritaria from "components/Shareable/ModalDataPrioritaria";
-import { MultiselectRaw } from "components/Shareable/MultiselectRaw";
 import { Select } from "components/Shareable/Select";
 import { toastError, toastSuccess } from "components/Shareable/Toast/dialogs";
 import { STATUS_DRE_A_VALIDAR } from "configs/constants";
@@ -17,8 +14,6 @@ import { TIPO_SOLICITACAO } from "constants/shared";
 import arrayMutators from "final-form-arrays";
 import {
   composeValidators,
-  maxValue,
-  naoPodeSerZero,
   peloMenosUmCaractere,
   required,
   textAreaRequired,
@@ -26,16 +21,11 @@ import {
 import {
   checaSeDataEstaEntre2e5DiasUteis,
   deepCopy,
-  fimDoCalendario,
   getError,
-  usuarioEhEscolaCeuGestao,
-  usuarioEhEscolaCMCT,
 } from "helpers/utilities";
 import HTTP_STATUS from "http-status-codes";
-import moment from "moment";
-import React, { Fragment, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Field, Form } from "react-final-form";
-import { FieldArray } from "react-final-form-arrays";
 import {
   escolaAlterarSolicitacaoDeAlteracaoCardapio,
   escolaCriarSolicitacaoDeAlteracaoCardapio,
@@ -43,21 +33,16 @@ import {
   escolaIniciarSolicitacaoDeAlteracaoDeCardapio,
   getRascunhosAlteracaoTipoAlimentacao,
 } from "services/alteracaoDeCardapio";
-import { getDiasUteis } from "services/diasUteis.service";
-import { formataValues } from "../helper";
-import ModalConfirmaAlteracao from "./components/ModalConfirmaAlteracao";
+import { formataValues } from "./helper";
+import { AlterarDiaOuPeriodo } from "./components/AlterarDiaOuPeriodoFields";
+import { PeriodosFields } from "./components/PeriodosFields";
 import { Rascunhos } from "./components/Rascunhos";
 import { validateSubmit } from "./components/validacao";
 import "./style.scss";
 
 export const AlteracaoCardapio = ({ ...props }) => {
   const [rascunhos, setRascunhos] = useState();
-  const [limiteDataInicial, setLimiteDataInicial] = useState();
-  const [limiteDataFinal, setLimiteDataFinal] = useState();
-
   const [showModalDiasUteis, setShowModalDiasUteis] = useState(false);
-  const [showModalConfirmarAlteracao, setShowModalConfirmarAlteracao] =
-    useState(false);
 
   const [erro, setErro] = useState("");
 
@@ -204,42 +189,6 @@ export const AlteracaoCardapio = ({ ...props }) => {
       ?.nome.includes(nome);
   };
 
-  const exibeApenasPorNome = (tiposAlimentacao, nome) => {
-    return tiposAlimentacao.filter((tp) => tp.label.includes(nome));
-  };
-
-  const removePorNome = (tiposAlimentacao, nome) => {
-    return tiposAlimentacao.filter((tp) => !tp.label.includes(nome));
-  };
-
-  const handleTiposAlimentacaoDe = (tiposAlimentacao, values) => {
-    if (ehMotivoPorNome("Lanche Emergencial", values))
-      return removePorNome(tiposAlimentacao, "Lanche Emergencial");
-    else if (ehMotivoPorNome("LPR", values)) {
-      return removePorNome(
-        exibeApenasPorNome(tiposAlimentacao, "Lanche"),
-        "Lanche Emergencial"
-      );
-    } else if (ehMotivoPorNome("RPL", values)) {
-      return removePorNome(tiposAlimentacao, "Lanche");
-    }
-    return tiposAlimentacao;
-  };
-
-  const handleTiposAlimentacaoPara = (tiposAlimentacao, values) => {
-    if (ehMotivoPorNome("Lanche Emergencial", values))
-      return exibeApenasPorNome(tiposAlimentacao, "Lanche Emergencial");
-    else if (ehMotivoPorNome("LPR", values)) {
-      return removePorNome(tiposAlimentacao, "Lanche");
-    } else if (ehMotivoPorNome("RPL", values)) {
-      return removePorNome(
-        exibeApenasPorNome(tiposAlimentacao, "Lanche"),
-        "Lanche Emergencial"
-      );
-    }
-    return tiposAlimentacao;
-  };
-
   const onAlterarDiaChanged = (value, values) => {
     if (
       value &&
@@ -252,34 +201,6 @@ export const AlteracaoCardapio = ({ ...props }) => {
     ) {
       setShowModalDiasUteis(true);
     }
-  };
-
-  const obtemDataInicial = async (value) => {
-    setLimiteDataInicial(moment(value, "DD/MM/YYYY").add(1, "days")["_d"]);
-    const response = await getDiasUteis({ data: value });
-    if (response.status === HTTP_STATUS.OK) {
-      setLimiteDataFinal(
-        moment(response.data.data_apos_quatro_dias_uteis, "YYYY-MM-DD")._d
-      );
-    } else {
-      setErro(
-        "Erro ao carregar limite da data final da Alteração de dia de Cardápio"
-      );
-    }
-  };
-
-  const getPeriodo = (index) => {
-    return periodos[index];
-  };
-
-  const handleNumeroAlunosValidate = (index) => {
-    return usuarioEhEscolaCeuGestao() || usuarioEhEscolaCMCT()
-      ? composeValidators(naoPodeSerZero, required)
-      : composeValidators(
-          naoPodeSerZero,
-          required,
-          maxValue(getPeriodo(index).maximo_alunos)
-        );
   };
 
   return (
@@ -339,198 +260,19 @@ export const AlteracaoCardapio = ({ ...props }) => {
                     </section>
                     {values.motivo && (
                       <>
-                        <section className="section-form-datas mt-2">
-                          <Field
-                            component={InputComData}
-                            inputOnChange={(value) =>
-                              onAlterarDiaChanged(value, values)
-                            }
-                            name="alterar_dia"
-                            minDate={
-                              ehMotivoPorNome("Lanche Emergencial", values)
-                                ? moment().toDate()
-                                : proximosDoisDiasUteis
-                            }
-                            maxDate={fimDoCalendario()}
-                            label="Alterar dia"
-                            disabled={values.data_inicial || values.data_final}
-                            usarDirty
-                          />
-                          <>
-                            <div className="opcao-data">Ou</div>
-                            <Field
-                              component={InputComData}
-                              name="data_inicial"
-                              label="De"
-                              minDate={
-                                ehMotivoPorNome("Lanche Emergencial", values)
-                                  ? moment().toDate()
-                                  : proximosDoisDiasUteis
-                              }
-                              maxDate={fimDoCalendario()}
-                              disabled={
-                                values.alterar_dia ||
-                                !values.motivo ||
-                                !ehMotivoPorNome("Lanche Emergencial", values)
-                              }
-                              inputOnChange={async (value) => {
-                                await obtemDataInicial(value);
-                                onAlterarDiaChanged(value, values);
-                              }}
-                            />
-                            <Field
-                              component={InputComData}
-                              name="data_final"
-                              label="Até"
-                              disabled={
-                                !values.data_inicial || values.alterar_dia
-                              }
-                              minDate={limiteDataInicial}
-                              maxDate={limiteDataFinal}
-                            />
-                          </>
-                        </section>
-
-                        <section>
-                          <div className="row mt-3 mb-3 g-0">
-                            <div className="col-3 pe-3">Período</div>
-                            <div className="col-3 pe-3">
-                              Alterar alimentação de:
-                            </div>
-                            <div className="col-3 pe-3">Para alimentação:</div>
-                            <div className="col-3">Nº de Alunos</div>
-                          </div>
-                          <FieldArray name="substituicoes">
-                            {({ fields }) =>
-                              fields.map((name, index) => (
-                                <div className="row g-0" key={index}>
-                                  <div className="col-3 pe-3">
-                                    <div
-                                      className={`period-quantity number-${index} ps-5 pt-2 pb-2`}
-                                    >
-                                      <Fragment>
-                                        <label
-                                          htmlFor="check"
-                                          className="checkbox-label"
-                                        >
-                                          <Field
-                                            component={"input"}
-                                            type="checkbox"
-                                            name={`${name}.check`}
-                                          />
-                                          <span
-                                            onClick={() => {
-                                              form.change(
-                                                `${name}.check`,
-                                                !values.substituicoes[index][
-                                                  "check"
-                                                ]
-                                              );
-                                            }}
-                                            className="checkbox-custom"
-                                            data-cy={`checkbox-${
-                                              getPeriodo(index).nome
-                                            }`}
-                                          />
-                                          <div className="">
-                                            {" "}
-                                            {getPeriodo(index).nome}
-                                          </div>
-                                        </label>
-                                      </Fragment>
-                                    </div>
-                                  </div>
-                                  <div className="col-3 pe-3">
-                                    <Field
-                                      component={MultiselectRaw}
-                                      name={`${name}.tipos_alimentacao_de`}
-                                      selected={
-                                        form.getState().values.substituicoes[
-                                          index
-                                        ].tipos_alimentacao_de_selecionados ||
-                                        []
-                                      }
-                                      options={handleTiposAlimentacaoDe(
-                                        getPeriodo(index).tipos_alimentacao.map(
-                                          (tipo_alimentacao) => ({
-                                            label: tipo_alimentacao.nome,
-                                            value: tipo_alimentacao.uuid,
-                                          })
-                                        ),
-                                        values
-                                      )}
-                                      onSelectedChanged={(values_) => {
-                                        form.change(
-                                          `substituicoes[${index}].tipos_alimentacao_de_selecionados`,
-                                          values_.map((value_) => value_.value)
-                                        );
-                                      }}
-                                      placeholder="Selecione tipos de alimentação"
-                                      disabled={
-                                        !values.substituicoes[index]["check"]
-                                      }
-                                      required={
-                                        values.substituicoes[index]["check"]
-                                      }
-                                    />
-                                  </div>
-                                  <div className="col-3 pe-3">
-                                    <Field
-                                      component={MultiselectRaw}
-                                      name={`${name}.tipos_alimentacao_para`}
-                                      selected={
-                                        values.substituicoes[index]
-                                          .tipos_alimentacao_para_selecionados ||
-                                        []
-                                      }
-                                      required={
-                                        values.substituicoes[index]["check"]
-                                      }
-                                      options={handleTiposAlimentacaoPara(
-                                        getPeriodo(index).tipos_alimentacao.map(
-                                          (tipo_alimentacao) => ({
-                                            label: tipo_alimentacao.nome,
-                                            value: tipo_alimentacao.uuid,
-                                          })
-                                        ),
-                                        values
-                                      )}
-                                      onSelectedChanged={(values_) => {
-                                        form.change(
-                                          `substituicoes[${index}].tipos_alimentacao_para_selecionados`,
-                                          values_.map((value_) => value_.value)
-                                        );
-                                      }}
-                                      placeholder="Selecione tipos de alimentação"
-                                      disabled={
-                                        !values.substituicoes[index]["check"]
-                                      }
-                                    />
-                                  </div>
-                                  <div className="col-3">
-                                    <Field
-                                      component={InputText}
-                                      disabled={
-                                        !values.substituicoes[index]["check"]
-                                      }
-                                      type="number"
-                                      name={`${name}.qtd_alunos`}
-                                      min="0"
-                                      step="1"
-                                      required={
-                                        values.substituicoes[index]["check"]
-                                      }
-                                      validate={
-                                        values.substituicoes[index]["check"] &&
-                                        handleNumeroAlunosValidate(index)
-                                      }
-                                    />
-                                  </div>
-                                </div>
-                              ))
-                            }
-                          </FieldArray>
-                        </section>
+                        <AlterarDiaOuPeriodo
+                          values={values}
+                          onAlterarDiaChanged={onAlterarDiaChanged}
+                          ehMotivoPorNome={ehMotivoPorNome}
+                          setErro={setErro}
+                          proximosDoisDiasUteis={proximosDoisDiasUteis}
+                        />
+                        <PeriodosFields
+                          ehMotivoPorNome={ehMotivoPorNome}
+                          periodos={periodos}
+                          form={form}
+                          values={values}
+                        />
                         <hr />
                         <Field
                           component={CKEditorField}
@@ -580,12 +322,6 @@ export const AlteracaoCardapio = ({ ...props }) => {
                 <ModalDataPrioritaria
                   showModal={showModalDiasUteis}
                   closeModal={() => setShowModalDiasUteis(false)}
-                />
-                <ModalConfirmaAlteracao
-                  showModal={showModalConfirmarAlteracao}
-                  closeModal={() => setShowModalConfirmarAlteracao(true)}
-                  values={values}
-                  onSubmit={onSubmit}
                 />
               </form>
             )}
