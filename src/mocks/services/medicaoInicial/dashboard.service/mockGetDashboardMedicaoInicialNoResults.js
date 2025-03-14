253b4f71537e1c119860aafd4b744015ee933632
vferreira90 <vef_90kl@hotmail.com>
2025-03-14 09:36:47 -0300

    bugfix(125745): Erro ao voltar na tela de companhamento dos lancamentos e testes unitarios

---
+++ src/mocks/services/medicaoInicial/dashboard.service/mockGetDashboardMedicaoInicialNoResults.js
@@ -0,0 +1,34 @@
+export const mockGetDashboardMedicaoInicialNoresults = {
+  results: [
+    {
+      status: "MEDICAO_EM_ABERTO_PARA_PREENCHIMENTO_UE",
+      total: 0,
+      dados: [],
+    },
+    {
+      status: "MEDICAO_APROVADA_PELA_DRE",
+      total: 0,
+      dados: [],
+    },
+    {
+      status: "MEDICAO_CORRECAO_SOLICITADA_CODAE",
+      total: 0,
+      dados: [],
+    },
+    {
+      status: "MEDICAO_CORRIGIDA_PARA_CODAE",
+      total: 0,
+      dados: [],
+    },
+    {
+      status: "MEDICAO_APROVADA_PELA_CODAE",
+      total: 0,
+      dados: [],
+    },
+    {
+      status: "TODOS_OS_LANCAMENTOS",
+      total: 0,
+      dados: [],
+    },
+  ],
+};
