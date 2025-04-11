1b14ae01dac7e9077922860dc2d79df918c4e687
Priscyla C. dos Santos <santospriscyla@gmail.com>
2025-04-11 15:44:04 -0300

    test(126945): Corrige a página do relatório do histórico de dietas

--- src/pages/DietaEspecial/__tests__/teste_RelatorioHistoricoDietasPage.js
+++ src/pages/DietaEspecial/__tests__/teste_RelatorioHistoricoDietasPage.js
@@ -41,7 +41,7 @@ describe("Teste - Relatório Histórico de Dietas Especiais", () => {
 
     mock
       .onGet("/solicitacoes-dieta-especial/relatorio-historico-dieta-especial/")
-      .replyOnce(200, mockGetSolicitacoesRelatorioHistoricoDietas);
+      .replyOnce(200, mockGetSolicitacoesRelatorioHistoricoDietas); //esse
 
     Object.defineProperty(global, "localStorage", { value: localStorageMock });
     localStorage.setItem("tipo_perfil", TIPO_PERFIL.ESCOLA);
@@ -64,21 +64,33 @@ describe("Teste - Relatório Histórico de Dietas Especiais", () => {
   it("renderiza título `Relatório de Histórico de Dietas`", async () => {
     expect(
       screen.getByText(
-        "Resultado da pesquisa - TOTAL DE DIETAS AUTORIZADAS EM 24/08/2023: 198"
+        "Resultado da pesquisa - TOTAL DE DIETAS AUTORIZADAS EM 12/02/2024: 27"
       )
     ).toBeInTheDocument();
   });
 
   it("Verifica se o botão para abrir o collaps está funcional", async () => {
-    const angleDownIcon = document.querySelector(".fa-angle-down");
+    const angleDownIcon = document.querySelectorAll(".fa-angle-down");
 
-    fireEvent.click(angleDownIcon);
-
+    const escolaCei = angleDownIcon[3];
+    fireEvent.click(escolaCei);
     expect(
       screen.getByText("Faixas Etárias com Dietas Autorizadas")
     ).toBeInTheDocument();
     expect(screen.getByText("Período INTEGRAL")).toBeInTheDocument();
-    expect(screen.getByText("Período PARCIAL")).toBeInTheDocument();
+    expect(screen.getByText("07 a 11 meses")).toBeInTheDocument();
+    expect(screen.getByText("01 ano a 03 anos e 11 meses")).toBeInTheDocument();
+
+    const escolaCeuCemei = angleDownIcon[7];
+    fireEvent.click(escolaCeuCemei);
+    expect(
+      screen.getByText("Dietas Autorizadas nas Turmas do Infantil")
+    ).toBeInTheDocument();
+    expect(screen.getByText("INTEGRAL")).toBeInTheDocument();
+
+    const escolaEmef = angleDownIcon[9];
+    fireEvent.click(escolaEmef);
+    expect(screen.getByText("MANHA")).toBeInTheDocument();
   });
 
   it("Verifica mudança de página e Collapse CEMEI", async () => {
@@ -99,10 +111,18 @@ describe("Teste - Relatório Histórico de Dietas Especiais", () => {
 
     const angleDownIcon = document.querySelector(".fa-angle-down");
     fireEvent.click(angleDownIcon);
+    expect(
+      screen.getByText("Faixas Etárias com Dietas Autorizadas")
+    ).toBeInTheDocument();
+    expect(screen.getByText("Período INTEGRAL")).toBeInTheDocument();
+    expect(screen.getByText("01 a 03 meses")).toBeInTheDocument();
 
     expect(
       screen.getByText("Dietas Autorizadas nas Turmas do Infantil")
     ).toBeInTheDocument();
+    expect(screen.getByText("INTEGRAL")).toBeInTheDocument();
+    expect(screen.getByText("MANHA")).toBeInTheDocument();
+    expect(screen.getByText("TARDE")).toBeInTheDocument();
   });
 
   it("Testa Collapse EMEBS e CEU GESTAO", async () => {
@@ -110,28 +130,33 @@ describe("Teste - Relatório Histórico de Dietas Especiais", () => {
       .onGet("/solicitacoes-dieta-especial/relatorio-historico-dieta-especial/")
       .replyOnce(200, mockGetHistoricoDietasEMEBSeCEUGESTAO);
 
-    const paginaTres = document.querySelector(
-      ".ant-pagination .ant-pagination-item-3"
+    const paginaDois = document.querySelector(
+      ".ant-pagination .ant-pagination-item-2"
     );
-    fireEvent.click(paginaTres);
+    fireEvent.click(paginaDois);
 
     await waitFor(() => {
+      expect(
+        screen.getAllByText("EMEBS NEUSA BASSETTO, PROFA.").length
+      ).toBeGreaterThan(0);
       expect(
         screen.getAllByText(
           "CEU GESTAO MENINOS - ARTUR ALBERTO DE MOTA GONCALVES, PROF. PR."
         ).length
       ).toBeGreaterThan(0);
-      expect(
-        screen.getAllByText("EMEBS VERA LUCIA APARECIDA RIBEIRO, PROFA.").length
-      ).toBeGreaterThan(0);
     });
 
     const angleDownIcon = document.querySelector(".fa-angle-down");
     fireEvent.click(angleDownIcon);
-
     expect(
       screen.getByText("Alunos do Infantil (4 a 6 anos)")
     ).toBeInTheDocument();
+    expect(screen.getByText("MANHA")).toBeInTheDocument();
+
+    expect(
+      screen.getByText("Alunos do Fundamental (acima de 6 anos)")
+    ).toBeInTheDocument();
+    expect(screen.getByText("TARDE")).toBeInTheDocument();
   });
 
   it("Verifica mudança de página e Collapse EMEI/EMEF", async () => {
@@ -139,10 +164,10 @@ describe("Teste - Relatório Histórico de Dietas Especiais", () => {
       .onGet("/solicitacoes-dieta-especial/relatorio-historico-dieta-especial/")
       .replyOnce(200, mockGetSolicitacoesHistoricoDietasEMEF);
 
-    const paginaQuatro = document.querySelector(
-      ".ant-pagination .ant-pagination-item-4"
+    const pagina_dois = document.querySelector(
+      ".ant-pagination .ant-pagination-item-2"
     );
-    fireEvent.click(paginaQuatro);
+    fireEvent.click(pagina_dois);
 
     await waitFor(() => {
       expect(
@@ -152,5 +177,7 @@ describe("Teste - Relatório Histórico de Dietas Especiais", () => {
 
     const angleDownIcon = document.querySelector(".fa-angle-down");
     fireEvent.click(angleDownIcon);
+    expect(screen.getByText("TARDE")).toBeInTheDocument();
+    expect(screen.getByText("MANHA")).toBeInTheDocument();
   });
 });
