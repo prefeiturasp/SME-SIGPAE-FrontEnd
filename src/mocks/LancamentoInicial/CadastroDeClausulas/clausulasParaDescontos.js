5f86d6c6dfccbdaced3cfa9510cbee65c57792a8
vferreira90 <vef_90kl@hotmail.com>
2025-05-16 11:08:54 -0300

    feature(128023): cobertura de testes para clausulas de desconto

---
+++ src/mocks/LancamentoInicial/CadastroDeClausulas/clausulasParaDescontos.js
@@ -0,0 +1,11 @@
+export const mockClausulaParaDesconto = {
+  edital: {
+    uuid: "3bf3c3c1-0651-48f9-ad53-73d8495d30c8",
+    numero: "23444",
+  },
+  uuid: "7b305126-ef6c-4974-881f-15a6e5893710",
+  numero_clausula: "2.5",
+  item_clausula: "A",
+  porcentagem_desconto: "22.00",
+  descricao: "RSHH RHRHKTYLUL EHSGSRGS",
+};
---
+++ src/mocks/LancamentoInicial/CadastroDeClausulas/listaDeNumeros.js
@@ -0,0 +1,116 @@
+export const mockListaNumeros = {
+  results: [
+    {
+      uuid: "752c11a3-b4fe-4f1c-b9af-61d42f0a6b56",
+      numero: "1",
+    },
+    {
+      uuid: "176294b8-7ac3-46a1-ae57-67da66d4fb4a",
+      numero: "PARCEIRA",
+    },
+    {
+      uuid: "0563c540-a7c4-48fa-9cb6-e81c2c43cccb",
+      numero: "456/SME2024",
+    },
+    {
+      uuid: "84deed35-980c-4975-939d-fff1ca45b605",
+      numero: "938373",
+    },
+    {
+      uuid: "3bf3c3c1-0651-48f9-ad53-73d8495d30c8",
+      numero: "23444",
+    },
+    {
+      uuid: "3dea0d3c-eea2-4f32-90a6-ebae3597374b",
+      numero: "132165453",
+    },
+    {
+      uuid: "fbe7a546-fba5-497b-b12e-160311f46222",
+      numero: "202020",
+    },
+    {
+      uuid: "e40ccae2-2080-4510-aeee-d1b8dcacc3b8",
+      numero: "41/SME/2017",
+    },
+    {
+      uuid: "fef87396-0d53-439a-b781-73320fcc48e3",
+      numero: "123456",
+    },
+    {
+      uuid: "e8fd49ae-a663-4398-9507-71cf359836ff",
+      numero: "SDFSFS",
+    },
+    {
+      uuid: "d5e05d2a-1d4f-456f-ac9a-07ecde54b1b3",
+      numero: "404040",
+    },
+    {
+      uuid: "31587b4e-6aed-48d6-913c-7d9939032f15",
+      numero: "Edital de Pregão n° 36/SME/2022",
+    },
+    {
+      uuid: "4ff22a0b-b70f-4636-9b67-6dfb0959f0df",
+      numero: "101010B",
+    },
+    {
+      uuid: "4ccd9282-1c57-465f-926a-798c72ed458d",
+      numero: "testando123",
+    },
+    {
+      uuid: "15fef71d-3fda-4d0f-bcd8-f38ebb54c627",
+      numero: "Edital de Pregão n.o 23/sme/2018",
+    },
+    {
+      uuid: "8b12fb07-ede8-4765-a5f7-56201a723e7a",
+      numero: "78/SME/2016",
+    },
+    {
+      uuid: "cdaf29df-3e86-499e-a125-b37b0fd704f1",
+      numero: "645766",
+    },
+    {
+      uuid: "a7790f00-53b0-4437-9100-f424f95f4c5a",
+      numero: "1234568",
+    },
+    {
+      uuid: "d23d1647-282a-4ea6-9bc0-85dcfa32cbc5",
+      numero: "paula",
+    },
+    {
+      uuid: "172e71fa-e0df-4842-a91c-4542d5778aae",
+      numero: "1235689",
+    },
+    {
+      uuid: "f76f367c-f9c4-463e-aefb-0ff434d93ae9",
+      numero: "303030A",
+    },
+    {
+      uuid: "bb110b71-a89d-4dfc-b7ea-5ba6c3418ace",
+      numero: "edital teste hom review",
+    },
+    {
+      uuid: "557d1c87-ea6c-4911-8876-2a133f754ea1",
+      numero: "EDITAL MODELO IMR",
+    },
+    {
+      uuid: "98e4a1d8-566a-47e0-81e7-1f657b5c33d8",
+      numero: "Edital SEPAT 2509",
+    },
+    {
+      uuid: "5d0c7b1b-bc5a-4f2d-9d46-a6916ac0790a",
+      numero: "EDITAL MODELO SEM IMR",
+    },
+    {
+      uuid: "ab7a7f99-8c9a-444e-846c-2e2ea5362c6c",
+      numero: "31/SME/CODAE/2023",
+    },
+    {
+      uuid: "d136caaa-db23-418a-81ec-c679e97c1197",
+      numero: "30/SME/CODAE/2023",
+    },
+    {
+      uuid: "ff94d604-4468-4553-9b54-6b428ce3be75",
+      numero: "Edital Alimentar 2505",
+    },
+  ],
+};
