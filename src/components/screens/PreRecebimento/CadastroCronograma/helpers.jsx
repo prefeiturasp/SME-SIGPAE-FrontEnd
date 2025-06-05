f340b10fd71630a73a116dfd05ed3526aca8c68b
Luis Henrique Zimmermann <lhzimmermann@gmail.com>
2025-06-05 10:38:36 -0300

    feat(128342): altera tipo de campo etapa nas etapas de cronogramas

--- src/components/screens/PreRecebimento/CadastroCronograma/helpers.jsx
+++ src/components/screens/PreRecebimento/CadastroCronograma/helpers.jsx
@@ -65,7 +65,7 @@ export const formataPayload = (
     numero_empenho: values[`empenho_${index}`],
     qtd_total_empenho:
       stringDecimalToNumber(values[`qtd_total_empenho_${index}`]) || undefined,
-    etapa: values[`etapa_${index}`],
+    etapa: parseInt(values[`etapa_${index}`]?.replace("Etapa ", "") || 0, 10),
     parte: values[`parte_${index}`],
     data_programada: values[`data_programada_${index}`]
       ? moment(values[`data_programada_${index}`], "DD/MM/YYYY").format(
