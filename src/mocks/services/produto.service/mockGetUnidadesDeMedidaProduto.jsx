57ca24457c4c0ab9d36a2a563a7d8779f842c860
Murilo Ferri Schirmer <murilo.schirmer@spassu.com.br>
2025-06-11 11:37:21 -0300

    test(124252): :white_check_mark: criação de teste unitário - AtualizacaoProdutoFormPage (USUARIO_EMPRESA)

---
+++ src/mocks/services/produto.service/mockGetUnidadesDeMedidaProduto.jsx
@@ -0,0 +1,44 @@
+export const mockGetUnidadesDeMedidaProduto = {
+  results: [
+    {
+      uuid: "c4de1e75-4b72-4bb8-a845-48017e557214",
+      nome: "LITRO",
+    },
+    {
+      uuid: "e555c435-1fab-4023-8ee1-ad27d7294b00",
+      nome: "GRAMA",
+    },
+    {
+      uuid: "616d2413-008d-48ff-8492-aad49ea62041",
+      nome: "KILOGRAMA",
+    },
+    {
+      uuid: "234cf6cf-9b9a-459b-ad78-ff9e21e0d6ed",
+      nome: "CAIXA",
+    },
+    {
+      uuid: "80fda1df-40ee-4e03-8414-0144b11733a3",
+      nome: "METRO",
+    },
+    {
+      uuid: "d9cb8687-b9a1-40cc-8f56-4ab5ae8d11b1",
+      nome: "UNIDADE",
+    },
+    {
+      uuid: "dec569bf-d7c6-4de7-a351-b68ce0651f60",
+      nome: "PACOTE",
+    },
+    {
+      uuid: "0e5df495-4761-4e83-95fd-59b4d8c67104",
+      nome: "TESTE",
+    },
+    {
+      uuid: "7359bfdc-c5a2-49ba-a0ac-ace56c99bf24",
+      nome: "CAIXA",
+    },
+    {
+      uuid: "3ddc62e1-a1a8-4fdb-8256-96d17f41e223",
+      nome: "KILOGRAMAS",
+    },
+  ],
+};
