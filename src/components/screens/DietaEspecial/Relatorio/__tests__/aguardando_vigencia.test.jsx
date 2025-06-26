2af88dde447973eaa304dc0e75f16b19d6607f57
Priscyla C. dos Santos <santospriscyla@gmail.com>
2025-06-26 12:08:53 -0300

    test(129837): teste do relatório do card aguardando vigência

---
+++ src/components/screens/DietaEspecial/Relatorio/__tests__/aguardando_vigencia.test.jsx
@@ -0,0 +1,172 @@
+import React from "react";
+
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { render, screen, waitFor, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Relatorio from "../";
+import {
+  respostaApiCancelamentoporDataTermino,
+  alergiasIntolerantes,
+  motivosNegacao,
+  classificacoesDieta,
+  listaProtocolosLiberados,
+  alimentos,
+  solicitacoesDietaEspecial,
+  protocoloPadraoDietaEspecial,
+} from "../dados";
+import { API_URL } from "src/constants/config";
+import mock from "src/services/_mock";
+import { VISAO, PERFIL } from "src/constants/shared";
+
+const payload = {
+  ...respostaApiCancelamentoporDataTermino(),
+  status_solicitacao: "CODAE_AUTORIZADO",
+  tipo_solicitacao: "ALTERACAO_UE",
+};
+
+const server = setupServer(
+  rest.get(
+    `${API_URL}/solicitacoes-dieta-especial/${payload.uuid}/`,
+    (req, res, ctx) => {
+      return res(ctx.json(payload));
+    }
+  ),
+  rest.get(`${API_URL}/motivos-negacao/`, (req, res, ctx) => {
+    return res(ctx.json(motivosNegacao()));
+  }),
+  rest.get(`${API_URL}/alergias-intolerancias/`, (req, res, ctx) => {
+    return res(ctx.json(alergiasIntolerantes()));
+  }),
+  rest.get(`${API_URL}/classificacoes-dieta/`, (req, res, ctx) => {
+    return res(ctx.json(classificacoesDieta()));
+  }),
+  rest.get(
+    `${API_URL}/protocolo-padrao-dieta-especial/lista-protocolos-liberados/`,
+    (req, res, ctx) => {
+      return res(ctx.json(listaProtocolosLiberados()));
+    }
+  ),
+  rest.get(`${API_URL}/alimentos/`, (req, res, ctx) => {
+    return res(ctx.json(alimentos()));
+  }),
+  rest.get(`${API_URL}/solicitacoes-dieta-especial/`, (req, res, ctx) => {
+    return res(ctx.json(solicitacoesDietaEspecial()));
+  }),
+  rest.get(
+    `${API_URL}/protocolo-padrao-dieta-especial/${payload.protocolo_padrao}/`,
+    (req, res, ctx) => {
+      return res(ctx.json(protocoloPadraoDietaEspecial()));
+    }
+  )
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test("Relatorio aguardando início vigência", async () => {
+  const search = `?uuid=${payload.uuid}&ehInclusaoContinua=false&card=aguardando-inicio-vigencia`;
+  Object.defineProperty(window, "location", {
+    value: {
+      search: search,
+    },
+  });
+  render(<Relatorio visao={VISAO.TERCEIRIZADA} />);
+
+  expect(
+    await screen.findByText(/dieta especial - Autorizada/i)
+  ).toBeInTheDocument();
+  expect(
+    await screen.findByRole("button", { name: /histórico/i })
+  ).toBeInTheDocument();
+  expect(await screen.queryByText("Motivo")).not.toBeInTheDocument();
+  expect(
+    await screen.queryByText("Justificativa da Negação")
+  ).not.toBeInTheDocument();
+
+  expect(await screen.getByText(/dados do aluno/i)).toBeInTheDocument();
+  expect(await screen.getByText(/código eol do aluno/i)).toBeInTheDocument();
+  expect(await screen.getByText(/data de nascimento/i)).toBeInTheDocument();
+  expect(await screen.getByText(/nome completo do aluno/i)).toBeInTheDocument();
+
+  expect(
+    await screen.getByText(/Dados da Escola de Destino/i)
+  ).toBeInTheDocument();
+  expect(
+    await screen.getByText(/dados da escola solicitante/i)
+  ).toBeInTheDocument();
+
+  expect(await screen.findAllByText("Nome")).toHaveLength(2);
+  expect(await screen.findAllByText("Telefone")).toHaveLength(2);
+  expect(await screen.findAllByText("E-mail")).toHaveLength(2);
+  expect(await screen.findAllByText("DRE")).toHaveLength(2);
+  expect(await screen.findAllByText("Lote")).toHaveLength(2);
+  expect(await screen.findAllByText("Tipo de Gestão")).toHaveLength(2);
+
+  expect(await screen.getByText(/Observações/i)).toBeInTheDocument();
+});
+
+test("Verifica botão de Gerar Protocolo - visão Tercerizada", async () => {
+  const search = `?uuid=${payload.uuid}&ehInclusaoContinua=false&card=aguardando-inicio-vigencia`;
+  Object.defineProperty(window, "location", {
+    value: {
+      search: search,
+    },
+  });
+
+  const mockPdfBlob = new Blob(["mocked PDF content"], {
+    type: "application/pdf",
+  });
+
+  global.URL.createObjectURL = jest.fn(() => "mock-url");
+
+  mock
+    .onGet(/\/solicitacoes-dieta-especial\/[^/]+\/protocolo\//)
+    .reply(200, mockPdfBlob);
+
+  render(<Relatorio visao={VISAO.TERCEIRIZADA} />);
+
+  localStorage.setItem("tipo_perfil", PERFIL.ADMINISTRADOR_EMPRESA);
+
+  await waitFor(() =>
+    expect(screen.getAllByText("Gerar Protocolo")).toHaveLength(1)
+  );
+  const buttons = screen.getAllByText("Gerar Protocolo");
+  const buttonGerarProtocolo = buttons[0].closest("button");
+  expect(buttonGerarProtocolo).toBeInTheDocument();
+
+  fireEvent.click(buttonGerarProtocolo);
+});
+
+test("Verifica botão de Marcar Conferencia - visão Tercerizada", async () => {
+  const search = `?uuid=${payload.uuid}&ehInclusaoContinua=false&card=aguardando-inicio-vigencia:`;
+  Object.defineProperty(window, "location", {
+    value: {
+      search: search,
+    },
+  });
+
+  const mockPdfBlob = new Blob(["mocked PDF content"], {
+    type: "application/pdf",
+  });
+
+  global.URL.createObjectURL = jest.fn(() => "mock-url");
+
+  mock
+    .onGet(/\/solicitacoes-dieta-especial\/[^/]+\/protocolo\//)
+    .reply(200, mockPdfBlob);
+
+  render(<Relatorio visao={VISAO.TERCEIRIZADA} />);
+
+  localStorage.setItem("tipo_perfil", PERFIL.ADMINISTRADOR_EMPRESA);
+
+  await waitFor(() =>
+    expect(screen.getAllByText("Marcar Conferência")).toHaveLength(1)
+  );
+  const buttons = screen.getAllByText("Marcar Conferência");
+  const buttonMarcarProtocolo = buttons[0].closest("button");
+  expect(buttonMarcarProtocolo).toBeInTheDocument();
+
+  fireEvent.click(buttonMarcarProtocolo);
+});
