9b4b46a5b46672c1c9e8971ef760a136e380d291
Calvin Rossinhole <calvin.masters@gmail.com>
2025-06-27 10:08:05 -0300

    test(129076): :white_check_mark: implementa esqueleto teste finaliza sem lancamentos

---
+++ src/mocks/services/medicaoInicial/solicitacaoMedicaoinicial.service/EMEF/Maio2025/quantidadesAlimentacaoesLancadasPeriodoGrupo.jsx
@@ -0,0 +1,76 @@
+export const quantidadesAlimentacaoesLancadasPeriodoGrupoEMEFMaio2025 = {
+  results: [
+    {
+      nome_periodo_grupo: "Solicitações de Alimentação",
+      status: "MEDICAO_EM_ABERTO_PARA_PREENCHIMENTO_UE",
+      justificativa: null,
+      valores: [
+        {
+          nome_campo: "lanche",
+          valor: 0,
+        },
+        {
+          nome_campo: "lanche_4h",
+          valor: 0,
+        },
+        {
+          nome_campo: "refeicao",
+          valor: 0,
+        },
+        {
+          nome_campo: "sobremesa",
+          valor: 0,
+        },
+      ],
+      valor_total: 0,
+    },
+    {
+      nome_periodo_grupo: "MANHA",
+      status: "MEDICAO_EM_ABERTO_PARA_PREENCHIMENTO_UE",
+      justificativa: null,
+      valores: [
+        {
+          nome_campo: "lanche",
+          valor: 0,
+        },
+        {
+          nome_campo: "lanche_4h",
+          valor: 0,
+        },
+        {
+          nome_campo: "refeicao",
+          valor: 0,
+        },
+        {
+          nome_campo: "sobremesa",
+          valor: 0,
+        },
+      ],
+      valor_total: 0,
+    },
+    {
+      nome_periodo_grupo: "Programas e Projetos",
+      status: "MEDICAO_EM_ABERTO_PARA_PREENCHIMENTO_UE",
+      justificativa: null,
+      valores: [
+        {
+          nome_campo: "lanche",
+          valor: 0,
+        },
+        {
+          nome_campo: "lanche_4h",
+          valor: 0,
+        },
+        {
+          nome_campo: "refeicao",
+          valor: 0,
+        },
+        {
+          nome_campo: "sobremesa",
+          valor: 0,
+        },
+      ],
+      valor_total: 0,
+    },
+  ],
+};
