b56d1a8e5ee63c5271d1e04a0622e87e55d09465
Priscyla C. dos Santos <santospriscyla@gmail.com>
2025-07-28 14:40:00 -0300

    test(130730): Inclui mocks da tela de solicitação de kit lanche

---
+++ src/components/SolicitacaoDeKitLanche/__tests__/teste_SolicitacaoESCOLA.jsx
@@ -0,0 +1,71 @@
+import "@testing-library/jest-dom";
+import React from "react";
+import mock from "src/services/_mock";
+import { act } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { mockMeusDadosEscolaEMEFPericles } from "src/mocks/meusDados/escolaEMEFPericles";
+import { APIMockVersion } from "src/mocks/apiVersionMock";
+import { mockGetMinhasSolicitacoes } from "src/mocks/SolicitacaokitLanche/mockGetMinhasSolicitacoes";
+import { mockKitLanche } from "src/mocks/SolicitacaokitLanche/mockKitLanche";
+import { mockDietasAtivasInativas } from "src/mocks/DietaEspecial/mockAtivasInativas";
+import SolicitacaoDeKitLanche from "src/components/SolicitacaoDeKitLanche/Container/base";
+import { renderWithProvider } from "src/utils/test-utils";
+import { localStorageMock } from "src/mocks/localStorageMock";
+
+describe("Teste de Solicitação de Kit Lanche", () => {
+  const escolaUuid =
+    mockMeusDadosEscolaEMEFPericles.vinculo_atual.instituicao.uuid;
+
+  beforeEach(async () => {
+    mock
+      .onGet("/usuarios/meus-dados/")
+      .reply(200, mockMeusDadosEscolaEMEFPericles);
+    mock
+      .onGet("/downloads/quantidade-nao-vistos/")
+      .reply(200, { quantidade_nao_vistos: 170 });
+    mock
+      .onGet("/notificacoes/")
+      .reply(200, {
+        next: null,
+        previous: null,
+        count: 0,
+        page_size: 4,
+        results: [],
+      });
+    mock.onGet("/api-version/").reply(200, APIMockVersion);
+    mock
+      .onGet("/downloads/quantidade-nao-vistos/")
+      .reply(200, { quantidade_nao_lidos: 0 });
+    mock.onGet(`/dias-uteis/?escola_uuid=${escolaUuid}/`).reply(200, {
+      proximos_cinco_dias_uteis: "2025-08-04",
+      proximos_dois_dias_uteis: "2025-07-31",
+    });
+    mock
+      .onGet("/solicitacoes-kit-lanche-avulsa/minhas-solicitacoes/")
+      .reply(200, mockGetMinhasSolicitacoes);
+    mock
+      .onGet("/solicitacoes-dieta-especial-ativas-inativas/")
+      .reply((config) => {
+        if (config.params.incluir_alteracao_ue === true) {
+          return [200, mockDietasAtivasInativas];
+        }
+      });
+    mock.onGet("/kit-lanches/").reply(200, mockKitLanche);
+
+    Object.defineProperty(global, "localStorage", { value: localStorageMock });
+    localStorage.setItem("eh_cemei", false);
+
+    await act(async () => {
+      renderWithProvider(
+        <MemoryRouter
+          future={{
+            v7_startTransition: true,
+            v7_relativeSplatPath: true,
+          }}
+        >
+          <SolicitacaoDeKitLanche meusDados={mockMeusDadosEscolaEMEFPericles} />
+        </MemoryRouter>
+      );
+    });
+  });
+});
