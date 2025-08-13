d42ca226d67f4af9f9f111518e3c718687e10a4d
Murilo Ferri Schirmer <murilo.schirmer@spassu.com.br>
2025-08-13 09:59:51 -0300

    feat(126063): :sparkles: adiciona lógica de Salvar e Assinar a Ficha de Recebimento

--- src/services/fichaRecebimento.service.ts
+++ src/services/fichaRecebimento.service.ts
@@ -6,6 +6,11 @@ import axios from "./_base";
 import { ResponseFichaRecebimento } from "src/interfaces/responses.interface";
 import { getMensagemDeErro } from "src/helpers/statusErrors";
 import { toastError } from "src/components/Shareable/Toast/dialogs";
+import { AxiosRequestConfig } from "axios";
+
+interface CustomAxiosRequestConfig extends AxiosRequestConfig {
+  skipAuthRefresh?: boolean;
+}
 
 export const cadastraRascunhoFichaRecebimento = async (
   payload: FichaRecebimentoPayload
@@ -14,6 +19,19 @@ export const cadastraRascunhoFichaRecebimento = async (
     return await axios.post("/rascunho-ficha-de-recebimento/", payload);
   } catch (error) {
     toastError(getMensagemDeErro(error.response.status));
+  }
+};
+
+export const cadastraFichaRecebimento = async (
+  payload: FichaRecebimentoPayload
+): Promise<ResponseFichaRecebimento> => {
+  try {
+    return await axios.post("/fichas-de-recebimento/", payload, {
+      skipAuthRefresh: true,
+    } as CustomAxiosRequestConfig);
+  } catch (error) {
+    if (error.response.status === 401) toastError(error.response.data[0]);
+    else toastError(getMensagemDeErro(error.response.status));
   }
 };
 
