0fb565758dc8913de103e410b62b46ff8c005f13
Priscyla C. dos Santos <santospriscyla@gmail.com>
2025-08-14 13:01:50 -0300

    test(132216): Incluindo testes que verificam os textos da tela

--- src/components/SolicitacaoDeKitLanche/__tests__/relatorioESCOLA.test.jsx
+++ src/components/SolicitacaoDeKitLanche/__tests__/relatorioESCOLA.test.jsx
@@ -63,8 +63,99 @@ describe("Teste Relatorio - Visão ESCOLA", () => {
     });
   });
 
-  it("AAAA", async () => {
+  it("Verifica as informações da Escola", async () => {
+    expect(
+      screen.getByText(`# ${mockKitLancheAvulsa.id_externo}`)
+    ).toBeInTheDocument();
+    expect(screen.getByText(/Nº DA SOLICITAÇÃO/i)).toBeInTheDocument();
+    expect(screen.getByText(/Escola Solicitante/i)).toBeInTheDocument();
+    expect(
+      screen.getByText(`${mockKitLancheAvulsa.escola.nome}`)
+    ).toBeInTheDocument();
+    expect(screen.getByText(/Código EOL/i)).toBeInTheDocument();
+    expect(
+      screen.getByText(`${mockKitLancheAvulsa.escola.codigo_eol}`)
+    ).toBeInTheDocument();
+    expect(screen.getAllByText(/DRE/i)).toHaveLength(3);
+    expect(
+      screen.getByText(`${mockKitLancheAvulsa.escola.diretoria_regional.nome}`)
+    ).toBeInTheDocument();
+    expect(screen.getByText(/Lote/i)).toBeInTheDocument();
+    expect(
+      screen.getByText(`${mockKitLancheAvulsa.escola.lote.nome}`)
+    ).toBeInTheDocument();
+    expect(screen.getByText(/Tipo de Gestão/i)).toBeInTheDocument();
+    expect(
+      screen.getByText(`${mockKitLancheAvulsa.escola.tipo_gestao.nome}`)
+    ).toBeInTheDocument();
+    expect(screen.getByText(/Empresa/i)).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        `${mockKitLancheAvulsa.rastro_terceirizada.nome_fantasia}`
+      )
+    ).toBeInTheDocument();
+  });
+
+  it("Verifica as informações da linha do tempo", async () => {
+    expect(screen.getByText(/Solicitação Realizada/i)).toBeInTheDocument();
+    expect(screen.getByText("29/09/2023 11:34:26")).toBeInTheDocument();
+    expect(screen.getByText(/RF: 8115257/i)).toBeInTheDocument();
+    expect(screen.getByText(/SUPER USUARIO ESCOLA EMEF/i)).toBeInTheDocument();
+
+    expect(screen.getByText(/DRE validou/i)).toBeInTheDocument();
+    expect(screen.getByText("29/09/2023 14:16:17")).toBeInTheDocument();
+    expect(screen.getByText(/RF: 0000010/i)).toBeInTheDocument();
+    expect(screen.getByText(/SUPER USUARIO DRE/i)).toBeInTheDocument();
+
+    expect(screen.getByText(/CODAE autorizou/i)).toBeInTheDocument();
+    expect(screen.getByText("29/09/2023 14:17:07")).toBeInTheDocument();
+    expect(screen.getByText(/RF: 0000011/i)).toBeInTheDocument();
+    expect(
+      screen.getByText(/CODAE Gestão de Terceirizadas/i)
+    ).toBeInTheDocument();
+  });
+
+  it("Verifica as informações de solicitaçoes similares", async () => {
+    expect(screen.getByText(/Solicitação Similar:/i)).toBeInTheDocument();
+  });
+
+  it("Verifica as informações do passeio", async () => {
     preview.debug();
-    expect(screen.getAllByText(/Nº DA SOLICITAÇÃO/i)).toHaveLength(1);
+    expect(screen.getByText(/Data do evento/i)).toBeInTheDocument();
+    expect(
+      screen.getByText(`${mockKitLancheAvulsa.solicitacao_kit_lanche.data}`)
+    ).toBeInTheDocument();
+    expect(screen.getByText(/Local do passeio/i)).toBeInTheDocument();
+    expect(
+      screen.getByText(`${mockKitLancheAvulsa.local}`)
+    ).toBeInTheDocument();
+    expect(screen.getByText("Evento/Atividade")).toBeInTheDocument();
+    expect(
+      screen.getByText(`${mockKitLancheAvulsa.evento}`)
+    ).toBeInTheDocument();
+    expect(screen.getByText(/Nº de Alunos/i)).toBeInTheDocument();
+    expect(
+      screen.getAllByText(`${mockKitLancheAvulsa.quantidade_alunos}`)
+    ).toHaveLength(2);
+    expect(screen.getByText(/Tempo Previsto de Passeio/i)).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        `${mockKitLancheAvulsa.solicitacao_kit_lanche.tempo_passeio_explicacao}`
+      )
+    ).toBeInTheDocument();
+    expect(screen.getByText(/Opção Desejada/i)).toBeInTheDocument();
+    expect(screen.getByText(/KIT 1/)).toBeInTheDocument();
+    expect(screen.getByText(/Nº Total de Kits/i)).toBeInTheDocument();
+  });
+
+  it("Verifica as informações adicionais", async () => {
+    preview.debug();
+    expect(screen.getByText(/Observações/i)).toBeInTheDocument();
+    expect(screen.getByText(/teste/i)).toBeInTheDocument();
+    expect(screen.getAllByText(/Autorizou/i)).toHaveLength(2);
+    expect(
+      screen.getByText("29/09/2023 14:17:07 - Informações da CODAE")
+    ).toBeInTheDocument();
+    expect(screen.getByText(/aprovado/i)).toBeInTheDocument();
   });
 });
