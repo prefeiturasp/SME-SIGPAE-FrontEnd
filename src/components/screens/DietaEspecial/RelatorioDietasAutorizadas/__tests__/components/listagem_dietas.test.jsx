ac0757a7f053c8cb3817ccdba968fcaf3c333ab8
GuiLMassini <guimassinifai@gmail.com>
2025-08-14 16:52:53 -0300

    test(132216): ref. a listagem dietas autorizadas

---
+++ src/components/screens/DietaEspecial/RelatorioDietasAutorizadas/__tests__/components/listagem_dietas.test.jsx
@@ -0,0 +1,67 @@
+import { act, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { MemoryRouter } from "react-router-dom";
+import { PERFIL, TIPO_PERFIL } from "src/constants/shared";
+import { MeusDadosContext } from "src/context/MeusDadosContext";
+import { mockMeusDadosCODAEGA } from "src/mocks/meusDados/CODAE-GA";
+import { mockRelatorioDietasEpeciais } from "src/mocks/services/dietaEspecial.service/relatorioDietasEspeciaisTerceirizada";
+import { ListagemDietas } from "../../components/ListagemDietas";
+import mock from "src/services/_mock";
+
+describe("Verifica componente de tabela relatório de dietas autorizadas", () => {
+  beforeEach(async () => {
+    mock
+      .onGet(
+        "/solicitacoes-dieta-especial/relatorio-dieta-especial-terceirizada/"
+      )
+      .reply(200, mockRelatorioDietasEpeciais);
+
+    localStorage.setItem(
+      "tipo_perfil",
+      TIPO_PERFIL.GESTAO_ALIMENTACAO_TERCEIRIZADA
+    );
+    localStorage.setItem(
+      "perfil",
+      PERFIL.COORDENADOR_GESTAO_ALIMENTACAO_TERCEIRIZADA
+    );
+
+    await act(async () => {
+      render(
+        <MemoryRouter
+          future={{
+            v7_startTransition: true,
+            v7_relativeSplatPath: true,
+          }}
+        >
+          <MeusDadosContext.Provider
+            value={{
+              meusDados: mockMeusDadosCODAEGA,
+              setMeusDados: jest.fn(),
+            }}
+          >
+            <ListagemDietas
+              dietasEspeciais={mockRelatorioDietasEpeciais}
+              meusDados={mockMeusDadosCODAEGA}
+              setDietasEspeciais={jest.fn()}
+              setLoadingDietas={jest.fn()}
+              values={{
+                status_selecionado: "AUTORIZADAS",
+              }}
+            />
+          </MeusDadosContext.Provider>
+        </MemoryRouter>
+      );
+    });
+  });
+
+  it("Verifica se o componente foi renderizado com todos campos e registros", () => {
+    expect(screen.getByText("DRE/Lote")).toBeInTheDocument();
+    expect(screen.getByText("Unidade Escolar")).toBeInTheDocument();
+    expect(
+      screen.getByText("4722212 - RHUAN ANGELLO FERREIRA ABREU")
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("8026992 - MARTIN ABREU GUIMARAES")
+    ).toBeInTheDocument();
+  });
+});
