bfafb96260ca4062247a83f8842e0f36e2fa60d0
GuiLMassini <guimassinifai@gmail.com>
2025-08-27 14:34:58 -0300

    test(133180): ref. a relatorio gerencial dietas

---
+++ src/components/screens/DietaEspecial/RelatorioGerencialDietas/__tests__/relatorio_gerencial_dietas.test.js
@@ -0,0 +1,91 @@
+import "@testing-library/jest-dom";
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { PERFIL } from "src/constants/shared";
+import { MeusDadosContext } from "src/context/MeusDadosContext";
+import { mockMeusDadosCODAEGA } from "src/mocks/meusDados/CODAE-GA";
+import RelatorioGerencialDietas from "src/pages/DietaEspecial/RelatorioGerencialDietas";
+import mock from "src/services/_mock";
+
+describe("Teste interface de Relatório Gerencial de Dietas", () => {
+  beforeEach(async () => {
+    mock.onGet("/usuarios/meus-dados/").reply(200, mockMeusDadosCODAEGA);
+    mock
+      .onGet("/nutrisupervisao-solicitacoes/totais-gerencial-dietas/")
+      .reply(200, {
+        total_solicitacoes: 36,
+        total_autorizadas: 33,
+        total_negadas: 0,
+        total_canceladas: 8,
+      });
+    mock
+      .onGet("/nutrisupervisao-solicitacoes/anos-com-dietas/")
+      .reply(200, [2024, 2025]);
+    localStorage.setItem("perfil", PERFIL.ADMINISTRADOR_SUPERVISAO_NUTRICAO);
+
+    await act(async () => {
+      render(
+        <MemoryRouter
+          future={{
+            v7_startTransition: true,
+            v7_relativeSplatPath: true,
+          }}
+        >
+          <MeusDadosContext.Provider
+            value={{
+              meusDados: mockMeusDadosCODAEGA,
+              setMeusDados: jest.fn(),
+            }}
+          >
+            <RelatorioGerencialDietas />
+          </MeusDadosContext.Provider>
+        </MemoryRouter>
+      );
+    });
+  });
+
+  it("Renderiza título e breadcrumb `Relatório Gerencial de Dietas`", () => {
+    expect(screen.queryAllByText("Relatório Gerencial de Dietas")).toHaveLength(
+      2
+    );
+  });
+
+  it("Verifica renderização componente de filtros", () => {
+    expect(screen.getByText("Filtrar Resultados")).toBeInTheDocument();
+    expect(screen.getByText("Ano")).toBeInTheDocument();
+  });
+
+  it("Seleciona datas e verifica comportamento do campo", () => {
+    const anoSelect = screen.getByText((content, element) => {
+      return (
+        element.classList.contains("select") &&
+        element.textContent?.includes("Ano")
+      );
+    });
+    expect(anoSelect).toBeInTheDocument();
+    fireEvent.mouseDown(anoSelect);
+
+    fireEvent.click(screen.getByText("2025"));
+    fireEvent.click(screen.getByText("2024"));
+
+    expect(screen.getByText("Todos os anos")).toBeInTheDocument();
+  });
+
+  it("Clica em filtrar e verifica exibição dos cards de totais", async () => {
+    const botao = screen.getByText("Filtrar").closest("button");
+    fireEvent.click(botao);
+
+    await waitFor(() => {
+      expect(screen.getByText("0036")).toBeInTheDocument();
+      expect(screen.getByText("0033")).toBeInTheDocument();
+      expect(screen.getByText("0000")).toBeInTheDocument();
+      expect(screen.getByText("0008")).toBeInTheDocument();
+    });
+  });
+});
