4250d0c050e116b4dd2abd738e5647068874d78b
Calvin Rossinhole <calvin.masters@gmail.com>
2025-10-15 17:32:52 -0300

    refactor(135898): :fire: remove funcoes inutilizadas

--- src/helpers/utilities.jsx
+++ src/helpers/utilities.jsx
@@ -25,15 +25,6 @@ import {
 //         - lidar com tipo de usuário ou perfil
 //       Os que não tiverem categoria definida podem ficar aqui
 
-export const showResults = (values) =>
-  new Promise((resolve) => {
-    setTimeout(() => {
-      // simulate server latency
-      window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`);
-      resolve();
-    }, 1500);
-  });
-
 export const dateDelta = (daysDelta) => {
   let today = new Date();
   today.setDate(today.getDate() + daysDelta);
