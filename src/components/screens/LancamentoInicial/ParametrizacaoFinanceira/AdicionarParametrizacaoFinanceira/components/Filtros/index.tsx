61b2ae10e3744647254f6b8fa4a5571595eaa953
GuiLMassini <guimassinifai@gmail.com>
2025-11-10 11:18:32 -0300

    feat(137162): validacao de data parametrizacao financeira

--- src/components/screens/LancamentoInicial/ParametrizacaoFinanceira/AdicionarParametrizacaoFinanceira/components/Filtros/index.tsx
+++ src/components/screens/LancamentoInicial/ParametrizacaoFinanceira/AdicionarParametrizacaoFinanceira/components/Filtros/index.tsx
@@ -1,5 +1,6 @@
+import moment from "moment";
 import React, { ChangeEvent, Dispatch, SetStateAction } from "react";
-import { Field } from "react-final-form";
+import { Field, FormSpy } from "react-final-form";
 import { FormApi } from "final-form";
 import { required } from "src/helpers/fieldValidators";
 import { Select } from "src/components/Shareable/Select";
@@ -87,29 +88,49 @@ export default (props: Props) => {
           disabled={uuidParametrizacao}
         />
       </div>
-      <div className="col-3">
-        <Field
-          dataTestId="data-inicial-input"
-          component={InputComData}
-          label="Período de Vigência"
-          name="data_inicial"
-          className="data-inicio"
-          placeholder="De"
-          validate={ehCadastro && required}
-          required={ehCadastro}
-        />
-      </div>
-      <div className="col-3">
-        <Field
-          dataTestId="data-final-input"
-          component={InputComData}
-          label="&nbsp;"
-          name="data_final"
-          className="data-final"
-          popperPlacement="bottom-end"
-          placeholder="Até"
-        />
-      </div>
+      <FormSpy subscription={{ values: true }}>
+        {({ values }) => {
+          return (
+            <>
+              <div className="col-3">
+                <Field
+                  dataTestId="data-inicial-input"
+                  component={InputComData}
+                  label="Período de Vigência"
+                  name="data_inicial"
+                  className="data-inicio"
+                  placeholder="De"
+                  validate={ehCadastro && required}
+                  required={ehCadastro}
+                  minDate={false}
+                  maxDate={
+                    values.data_final
+                      ? moment(values.data_final, "DD/MM/YYYY").toDate()
+                      : null
+                  }
+                />
+              </div>
+
+              <div className="col-3">
+                <Field
+                  dataTestId="data-final-input"
+                  component={InputComData}
+                  label="&nbsp;"
+                  name="data_final"
+                  className="data-final"
+                  popperPlacement="bottom-end"
+                  placeholder="Até"
+                  minDate={
+                    values.data_inicial
+                      ? moment(values.data_inicial, "DD/MM/YYYY").toDate()
+                      : null
+                  }
+                />
+              </div>
+            </>
+          );
+        }}
+      </FormSpy>
       {ehCadastro && (
         <div className="col-2 mt-1">
           <br />
