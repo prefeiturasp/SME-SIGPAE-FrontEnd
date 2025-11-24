64f546c09dc165d45ffe09fe689ed4d84f0cdeaf
Calvin Rossinhole <calvin.masters@gmail.com>
2025-11-24 11:40:16 -0300

    refactor(129339): :fire: remove funcoes inutilizadas

--- src/components/screens/DietaEspecial/Escola/helper.jsx
+++ src/components/screens/DietaEspecial/Escola/helper.jsx
@@ -3,27 +3,12 @@ import {
   USUARIO_PODE_ATUALIZAR_FOTO_PROD,
 } from "src/configs/constants";
 import { ENVIRONMENT } from "src/constants/config";
-import {
-  usuarioEhEscolaTerceirizadaDiretor,
-  usuarioEhEscolaTerceirizada,
-} from "src/helpers/utilities";
 
 export const formatarSolicitacoesVigentes = (solicitacoes) => {
   solicitacoes.forEach((solicitacao) => {
     solicitacao.active = false;
   });
   return solicitacoes;
-};
-
-export const exibirParteInativacao = (solicitacao, uuid) => {
-  return (
-    solicitacao.ativo &&
-    uuid &&
-    (usuarioEhEscolaTerceirizadaDiretor() || usuarioEhEscolaTerceirizada()) &&
-    ["CODAE_AUTORIZADO", "TERCEIRIZADA_TOMOU_CIENCIA"].includes(
-      solicitacao.status_solicitacao
-    )
-  );
 };
 
 export const podeAtualizarFoto = (criadoRF) => {
