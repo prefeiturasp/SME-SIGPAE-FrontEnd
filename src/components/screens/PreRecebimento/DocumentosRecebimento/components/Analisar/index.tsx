cfc6e60921e65cd1b63a40ef416109dba4ba647b
Murilo Ferri Schirmer <murilo.schirmer@spassu.com.br>
2025-12-26 11:30:54 -0300

    feat(139621): :sparkles: melhoria no campo de data de validade - documentos de recebimento

--- src/components/screens/PreRecebimento/DocumentosRecebimento/components/Analisar/index.tsx
+++ src/components/screens/PreRecebimento/DocumentosRecebimento/components/Analisar/index.tsx
@@ -22,6 +22,7 @@ import {
 import { getNomesEAbreviacoesUnidadesMedida } from "src/services/qualidade.service";
 import { getListaLaboratoriosCredenciados } from "src/services/laboratorio.service";
 import InputText from "src/components/Shareable/Input/InputText";
+import MaskedInputText from "src/components/Shareable/Input/MaskedInputText";
 import {
   DocumentosRecebimentoParaAnalise,
   OptionsGenerico,
@@ -537,15 +538,26 @@ export default () => {
                       </div>
                       <div className="col">
                         <Field
-                          component={InputComData}
+                          component={MaskedInputText}
                           label="Data de Validade"
                           name={`data_validade_${index}`}
                           placeholder="Selecione uma Data"
                           className="input-analise"
                           required
                           validate={required}
-                          minDate={new Date()}
                           disabled={documentoRecebimentoPassouPorAprovacao}
+                          mask={[
+                            /\d/,
+                            /\d/,
+                            "/",
+                            /\d/,
+                            /\d/,
+                            "/",
+                            /\d/,
+                            /\d/,
+                            /\d/,
+                            /\d/,
+                          ]}
                         />
                       </div>
                       <div className="col">
