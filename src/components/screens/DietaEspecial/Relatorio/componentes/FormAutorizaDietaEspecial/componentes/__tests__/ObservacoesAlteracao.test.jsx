b769315dc78b74a00aa9567fdf99442c930aa252
Priscyla C. dos Santos <santospriscyla@gmail.com>
2026-01-16 12:45:33 -0300

    test(140683): Testes do componente ModalEnviarAlteracao

--- src/components/screens/DietaEspecial/Relatorio/componentes/FormAutorizaDietaEspecial/componentes/__tests__/ObservacoesAlteracao.test.jsx
+++ src/components/screens/DietaEspecial/Relatorio/componentes/FormAutorizaDietaEspecial/componentes/__tests__/ObservacoesAlteracao.test.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import ObservacoesAlteracao from "src/components/screens/DietaEspecial/Relatorio/componentes/FormAutorizaDietaEspecial/componentes/ObservacoesAlteracao/index.jsx";
-import preview from "jest-preview";
 
 describe("Teste do componente ObservacoesAlteracao", () => {
   it("deve renderizar o título e o conteúdo HTML corretamente", () => {
     const htmlMock =
       "<strong>Texto em negrito</strong> e um <p> parágrafo.</p>";
     render(<ObservacoesAlteracao observacoes={htmlMock} />);
-    preview.debug();
     expect(screen.getByText("Observações da Alteração")).toBeInTheDocument();
     const containerHtml = screen.getByText((content, element) => {
       return element.classList.contains("orientacoes");
