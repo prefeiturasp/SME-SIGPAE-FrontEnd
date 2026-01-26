30ff86fed48d271c854dc5925977a30807d87537
GuiLMassini <guimassinifai@gmail.com>
2026-01-26 12:13:48 -0300

    feat(140174): nova tabela de alimentacoes cei

--- src/components/screens/LancamentoInicial/RelatorioFinanceiro/RelatorioConsolidado/index.tsx
+++ src/components/screens/LancamentoInicial/RelatorioFinanceiro/RelatorioConsolidado/index.tsx
@@ -1,50 +1,98 @@
-import React from "react";
+import "./styles.scss";
+import HTTP_STATUS from "http-status-codes";
+import { useEffect, useState } from "react";
 import { Form } from "react-final-form";
-
 import { Spin } from "antd";
 import { FormFields } from "../components/FormFields";
-
-import { TabelaAlimentacaoCEI } from "../components/TabelaAlimentacaoCEI";
+import { TabelaAlimentacaoCEI } from "../components/Tabelas/TabelaAlimentacaoCEI";
 import { TabelaDietasCEI } from "../components/TabelaDietasCEI";
-
-import useView from "../view";
-import "./styles.scss";
+import { useRelatorioFinanceiro } from "../view";
+import { useLocation } from "react-router-dom";
+import { getFaixasEtarias } from "src/services/faixaEtaria.service";
+import { toastError } from "src/components/Shareable/Toast/dialogs";
+import { FaixaEtaria } from "src/services/medicaoInicial/parametrizacao_financeira.interface";
+import { getTotaisAtendimentoConsumo } from "src/services/medicaoInicial/solicitacaoMedicaoInicial.service";
 
 export function RelatorioConsolidado() {
-  const view = useView({});
+  const [faixasEtarias, setFaixasEtarias] = useState<FaixaEtaria[]>([]);
+  const [totaisConsumo, setTotaisConsumo] = useState<any>([]);
+
+  const {
+    carregando,
+    lotes,
+    gruposUnidadeEscolar,
+    mesesAnos,
+    relatorioConsolidado,
+  } = useRelatorioFinanceiro();
+
+  const { state } = useLocation();
+
+  const getTodasFaixasEtarias = async () => {
+    const response = await getFaixasEtarias();
+    if (response.status === HTTP_STATUS.OK)
+      setFaixasEtarias(response.data.results);
+    else toastError("Erro ao carregar faixas etárias.");
+  };
+
+  const getTotaisConsumo = async () => {
+    const { mes_ano, lote, grupo_unidade_escolar, status } = state;
+    const [mes, ano] = mes_ano.split("_");
+
+    const response = await getTotaisAtendimentoConsumo({
+      mes: mes,
+      ano: ano,
+      lote: lote[0],
+      grupo_unidade_escolar: grupo_unidade_escolar[0],
+      status: status[0],
+    });
+
+    if (response.status === HTTP_STATUS.OK) setTotaisConsumo(response.data);
+    else toastError("Erro ao carregar totais de atendimento e consumo.");
+  };
+
+  useEffect(() => {
+    getTodasFaixasEtarias();
+    if (state) getTotaisConsumo();
+  }, [state]);
 
   return (
     <div className="relatorio-consolidado">
-      <Spin tip="Carregando..." spinning={view.carregando}>
+      <Spin tip="Carregando..." spinning={carregando}>
         <div className="card mt-3">
           <div className="card-body">
-            <Form onSubmit={() => {}} initialValues={view.valoresIniciais}>
+            <Form key={state} onSubmit={() => {}} initialValues={state}>
               {() => (
                 <form>
                   <FormFields
-                    lotes={view.lotes}
-                    gruposUnidadeEscolar={view.gruposUnidadeEscolar}
-                    mesesAnos={view.mesesAnos}
+                    lotes={lotes}
+                    gruposUnidadeEscolar={gruposUnidadeEscolar}
+                    mesesAnos={mesesAnos}
                   />
                 </form>
               )}
             </Form>
 
-            {!view.carregando && view.relatorioConsolidado ? (
+            {!carregando && relatorioConsolidado ? (
               <div className="tabelas-relatorio-consolidado mt-5 mb-4">
-                {view.relatorioConsolidado?.grupo_unidade_escolar.nome ===
+                {relatorioConsolidado?.grupo_unidade_escolar.nome ===
                 "Grupo 1" ? (
                   <>
                     <TabelaAlimentacaoCEI
-                      tabelas={view.relatorioConsolidado.tabelas}
+                      tabelas={relatorioConsolidado.tabelas}
+                      faixasEtarias={faixasEtarias}
+                      totaisConsumo={totaisConsumo}
                     />
                     <TabelaDietasCEI
-                      tabelas={view.relatorioConsolidado.tabelas}
-                      tipoDieta="Dietas Tipo A e Tipo A Enteral"
+                      tabelas={relatorioConsolidado.tabelas}
+                      tipoDieta="TIPO A"
+                      faixasEtarias={faixasEtarias}
+                      totaisConsumo={totaisConsumo}
                     />
                     <TabelaDietasCEI
-                      tabelas={view.relatorioConsolidado.tabelas}
-                      tipoDieta="Dietas Tipo B"
+                      tabelas={relatorioConsolidado.tabelas}
+                      tipoDieta="TIPO B"
+                      faixasEtarias={faixasEtarias}
+                      totaisConsumo={totaisConsumo}
                     />
                   </>
                 ) : null}
