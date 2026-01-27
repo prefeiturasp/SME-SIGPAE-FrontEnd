240fc48b444bc8cd54943a11630fccd0e3d3bf5a
GuiLMassini <guimassinifai@gmail.com>
2026-01-27 16:12:59 -0300

    feat(140214): carregamento tipos de alimentacao e exibicao grupoemei

--- src/components/screens/LancamentoInicial/RelatorioFinanceiro/RelatorioFinanceiroConsolidado/index.tsx
+++ src/components/screens/LancamentoInicial/RelatorioFinanceiro/RelatorioFinanceiroConsolidado/index.tsx
@@ -1,6 +1,6 @@
 import "./styles.scss";
 import HTTP_STATUS from "http-status-codes";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Form } from "react-final-form";
 import { Spin } from "antd";
 import { FormFields } from "../components/FormFields";
@@ -11,10 +11,14 @@ import { getFaixasEtarias } from "src/services/faixaEtaria.service";
 import { toastError } from "src/components/Shareable/Toast/dialogs";
 import { FaixaEtaria } from "src/services/medicaoInicial/parametrizacao_financeira.interface";
 import { getTotaisAtendimentoConsumo } from "src/services/medicaoInicial/solicitacaoMedicaoInicial.service";
+import GrupoEMEI from "../components/Tabelas/GrupoEMEI";
+import { getTiposUnidadeEscolarTiposAlimentacao } from "src/services/cadastroTipoAlimentacao.service";
+import { SelectOption } from "../types";
 
 export function RelatorioFinanceiroConsolidado() {
   const [faixasEtarias, setFaixasEtarias] = useState<FaixaEtaria[]>([]);
   const [totaisConsumo, setTotaisConsumo] = useState<any>([]);
+  const [tiposAlimentacao, setTiposAlimentacao] = useState<any[]>([]);
 
   const {
     carregando,
@@ -49,10 +53,85 @@ export function RelatorioFinanceiroConsolidado() {
     else toastError("Erro ao carregar totais de atendimento e consumo.");
   };
 
+  const getTiposUnidades = async () => {
+    const { data } = await getTiposUnidadeEscolarTiposAlimentacao();
+
+    const grupo = gruposUnidadeEscolar.find(
+      (e) => e.value === state.grupo_unidade_escolar[0],
+    ).label;
+
+    const match = grupo.match(/\((.*?)\)/);
+    let unidades: string[] = [];
+    if (match && match[1]) {
+      const tipos = match[1].split(",");
+      unidades = tipos.map((item) => item.trim());
+    }
+
+    const tiposAlimentacaoUnidades: Array<SelectOption> = unidades.reduce(
+      (acc, tipoUnidade) => {
+        acc.push(
+          ...data.results
+            .find((t) => t.iniciais === tipoUnidade)
+            .periodos_escolares.reduce((acc, periodo) => {
+              acc.push(...periodo.tipos_alimentacao);
+              return acc;
+            }, []),
+        );
+        return acc;
+      },
+      [],
+    );
+
+    const tiposAlimentacaoUnicos = {};
+
+    tiposAlimentacaoUnidades.forEach((tipoAlimentacao) => {
+      tiposAlimentacaoUnicos[tipoAlimentacao.uuid] = tipoAlimentacao.nome;
+    });
+
+    const tiposAlimentacao = Object.entries(tiposAlimentacaoUnicos).map(
+      ([uuid, nome]) => ({
+        uuid,
+        nome,
+      }),
+    );
+
+    setTiposAlimentacao(tiposAlimentacao);
+  };
+
   useEffect(() => {
     getTodasFaixasEtarias();
-    if (state) getTotaisConsumo();
+  }, []);
+
+  useEffect(() => {
+    if (!state) return;
+    getTotaisConsumo();
   }, [state]);
+
+  useEffect(() => {
+    if (!state?.grupo_unidade_escolar?.length || !gruposUnidadeEscolar?.length)
+      return;
+    getTiposUnidades();
+  }, [state, gruposUnidadeEscolar]);
+
+  const grupo =
+    relatorioConsolidado?.grupo_unidade_escolar?.nome?.toLowerCase();
+
+  const GRUPOS_POR_COMPONENTE: Record<string, React.ReactNode> = {
+    "grupo 1": (
+      <GrupoCEI
+        relatorioConsolidado={relatorioConsolidado}
+        faixasEtarias={faixasEtarias}
+        totaisConsumo={totaisConsumo}
+      />
+    ),
+    "grupo 3": (
+      <GrupoEMEI
+        relatorioConsolidado={relatorioConsolidado}
+        tiposAlimentacao={tiposAlimentacao}
+        totaisConsumo={totaisConsumo}
+      />
+    ),
+  };
 
   return (
     <div className="relatorio-consolidado">
@@ -72,18 +151,16 @@ export function RelatorioFinanceiroConsolidado() {
               )}
             </Form>
 
-            {!carregando && relatorioConsolidado ? (
+            {!carregando && relatorioConsolidado && (
               <div className="tabelas-relatorio-consolidado mt-5 mb-4">
-                {relatorioConsolidado?.grupo_unidade_escolar.nome ===
-                "Grupo 1" ? (
-                  <GrupoCEI
-                    relatorioConsolidado={relatorioConsolidado}
-                    faixasEtarias={faixasEtarias}
-                    totaisConsumo={totaisConsumo}
-                  />
-                ) : null}
+                {Object.entries(GRUPOS_POR_COMPONENTE).map(
+                  ([key, componente]) =>
+                    grupo?.includes(key) && (
+                      <React.Fragment key={key}>{componente}</React.Fragment>
+                    ),
+                )}
               </div>
-            ) : null}
+            )}
           </div>
         </div>
       </Spin>
