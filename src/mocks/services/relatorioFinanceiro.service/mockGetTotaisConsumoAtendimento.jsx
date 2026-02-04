37310f5b4bb4621173c0d4651de490ae0937acc1
GuiLMassini <guimassinifai@gmail.com>
2026-02-04 15:58:41 -0300

    test(140436): ref. a ateste financeiro

---
+++ src/mocks/services/relatorioFinanceiro.service/mockGetTotaisConsumoAtendimento.jsx
@@ -0,0 +1,29 @@
+export const mockTotaisAtendimentoFaixaEtaria = {
+  INTEGRAL: {
+    "00 meses": 6.0,
+    "01 a 03 meses": 6.0,
+    "04 a 05 meses": 6.0,
+    "06 meses": 6.0,
+    "07 a 11 meses": 6.0,
+    "01 ano a 03 anos e 11 meses": 6.0,
+    "04 anos a 06 anos": 6.0,
+  },
+  "DIETA ESPECIAL - TIPO B - INTEGRAL": {
+    "00 meses": 4.0,
+    "01 a 03 meses": 4.0,
+    "04 a 05 meses": 4.0,
+    "06 meses": 4.0,
+    "07 a 11 meses": 4.0,
+    "01 ano a 03 anos e 11 meses": 4.0,
+    "04 anos a 06 anos": 4.0,
+  },
+  "DIETA ESPECIAL - TIPO A - INTEGRAL": {
+    "00 meses": 4.0,
+    "01 a 03 meses": 4.0,
+    "04 a 05 meses": 4.0,
+    "06 meses": 4.0,
+    "07 a 11 meses": 4.0,
+    "01 ano a 03 anos e 11 meses": 4.0,
+    "04 anos a 06 anos": 4.0,
+  },
+};
