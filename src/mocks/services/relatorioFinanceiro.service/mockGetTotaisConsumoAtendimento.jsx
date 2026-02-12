5d1727abefe428c6fd3f12ebba59a34cdd94db67
GuiLMassini <guimassinifai@gmail.com>
2026-02-12 14:17:42 -0300

    test(140465): mock totais consumo tipo alimentacao

--- src/mocks/services/relatorioFinanceiro.service/mockGetTotaisConsumoAtendimento.jsx
+++ src/mocks/services/relatorioFinanceiro.service/mockGetTotaisConsumoAtendimento.jsx
@@ -27,3 +27,25 @@ export const mockTotaisAtendimentoFaixaEtaria = {
     "04 anos a 06 anos": 4.0,
   },
 };
+
+export const mockTotaisAtendimentoTipoAlimentacao = {
+  ALIMENTAÇÃO: {
+    total_refeicao: 2762,
+    total_sobremesa: 1891,
+    total_refeicao_eja: 54,
+    total_lanche: 2237.0,
+    total_lanche_4h: 2227.0,
+    total_kit_lanche: 40.0,
+    total_lanche_emergencial: 20.0,
+  },
+  "DIETA ESPECIAL - TIPO B": {
+    lanche: 10.0,
+    lanche_4h: 10.0,
+  },
+  "DIETA ESPECIAL - TIPO A": {
+    lanche: 17.0,
+    lanche_4h: 17.0,
+    refeicao: 5.0,
+    refeicao_eja: 18.0,
+  },
+};
