09c16330bd045625c62c2063d16accfc0be81fb1
GuiLMassini <guimassinifai@gmail.com>
2026-02-12 09:39:38 -0300

    feat(140465): consolidado total grupo 3

--- src/components/screens/LancamentoInicial/RelatorioFinanceiro/components/Tabelas/GrupoEMEI.tsx
+++ src/components/screens/LancamentoInicial/RelatorioFinanceiro/components/Tabelas/GrupoEMEI.tsx
@@ -1,7 +1,10 @@
+import { useEffect, useRef, useState } from "react";
 import { TabelaAlimentacao } from "./TabelaAlimentacao";
 import { TipoAlimentacao } from "src/services/medicaoInicial/parametrizacao_financeira.interface";
 import { TabelaDietas } from "./TabelaDietas";
 import { RelatorioFinanceiroConsolidado } from "src/interfaces/relatorio_financeiro.interface";
+import { TabelaAlimentacaoHandle, TabelaDietasHandle } from "../../types";
+import { ConsolidadoTotal } from "../ConsolidadoTotal";
 
 type Props = {
   relatorioConsolidado: RelatorioFinanceiroConsolidado;
@@ -14,15 +17,44 @@ export default ({
   tiposAlimentacao,
   totaisConsumo,
 }: Props) => {
+  const [consolidado, setConsolidado] = useState({
+    quantidade: 0,
+    valor: 0,
+  });
+
+  const refAlimentacao = useRef<TabelaAlimentacaoHandle>(null);
+  const refDietaA = useRef<TabelaDietasHandle>(null);
+  const refDietaB = useRef<TabelaDietasHandle>(null);
+
+  useEffect(() => {
+    const alimentacao = refAlimentacao.current?.getTotais();
+    const dietaA = refDietaA.current?.getTotais();
+    const dietaB = refDietaB.current?.getTotais();
+
+    setConsolidado({
+      quantidade:
+        (alimentacao?.totalAtendimentosGeral ?? 0) +
+        (dietaA?.totalConsumoGeral ?? 0) +
+        (dietaB?.totalConsumoGeral ?? 0),
+
+      valor:
+        (alimentacao?.valorTotalGeral ?? 0) +
+        (dietaA?.valorTotalGeral ?? 0) +
+        (dietaB?.valorTotalGeral ?? 0),
+    });
+  }, [relatorioConsolidado, tiposAlimentacao, totaisConsumo]);
+
   return (
     <div className="d-flex flex-column gap-4">
       <TabelaAlimentacao
+        ref={refAlimentacao}
         tabelas={relatorioConsolidado.tabelas}
         tiposAlimentacao={tiposAlimentacao}
         totaisConsumo={totaisConsumo}
         ordem="A"
       />
       <TabelaDietas
+        ref={refDietaA}
         tabelas={relatorioConsolidado.tabelas}
         tipoDieta="TIPO A"
         tiposAlimentacao={tiposAlimentacao}
@@ -30,11 +62,17 @@ export default ({
         ordem="B"
       />
       <TabelaDietas
+        ref={refDietaB}
         tabelas={relatorioConsolidado.tabelas}
         tipoDieta="TIPO B"
         tiposAlimentacao={tiposAlimentacao}
         totaisConsumo={totaisConsumo}
         ordem="C"
+      />
+      <ConsolidadoTotal
+        titulo="CONSOLIDADO TOTAL (A + B + C)"
+        quantidade={consolidado.quantidade}
+        valor={consolidado.valor}
       />
     </div>
   );
