1699d75515cfb0ed032cb846bf9096b49a8800ec
GuiLMassini <guimassinifai@gmail.com>
2026-03-09 14:44:58 -0300

    feat(140467): exibicao consolidado grupo 5

---
+++ src/components/screens/LancamentoInicial/RelatorioFinanceiro/components/Tabelas/GrupoEMEBS.tsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import { ConsolidadoTotal } from "../ConsolidadoTotal";
+import { TabelaAlimentacaoHandle, TabelaDietasHandle } from "../../types";
+
+export default () => {
+  const [consolidadoInfantil, setConsolidadoCEI] = useState({
+    quantidade: 0,
+    valor: 0,
+  });
+
+  const [consolidadoFundamental, setConsolidadoEMEI] = useState({
+    quantidade: 0,
+    valor: 0,
+  });
+
+  const refAlimentacaoEMEI = useRef<TabelaAlimentacaoHandle>(null);
+  const refDietaAEMEI = useRef<TabelaDietasHandle>(null);
+  const refDietaBEMEI = useRef<TabelaDietasHandle>(null);
+
+  const refAlimentacaoCEI = useRef<TabelaAlimentacaoHandle>(null);
+  const refDietaACEI = useRef<TabelaDietasHandle>(null);
+  const refDietaBCEI = useRef<TabelaDietasHandle>(null);
+
+  const calcularConsolidado = (
+    refAlimentacao: React.RefObject<TabelaAlimentacaoHandle>,
+    refDietaA: React.RefObject<TabelaDietasHandle>,
+    refDietaB: React.RefObject<TabelaDietasHandle>,
+  ) => {
+    const alimentacao = refAlimentacao.current?.getTotais();
+    const dietaA = refDietaA.current?.getTotais();
+    const dietaB = refDietaB.current?.getTotais();
+
+    return {
+      quantidade:
+        (alimentacao?.totalAtendimentosGeral ?? 0) +
+        (dietaA?.totalConsumoGeral ?? 0) +
+        (dietaB?.totalConsumoGeral ?? 0),
+
+      valor:
+        (alimentacao?.valorTotalGeral ?? 0) +
+        (dietaA?.valorTotalGeral ?? 0) +
+        (dietaB?.valorTotalGeral ?? 0),
+    };
+  };
+
+  useEffect(() => {
+    const cei = calcularConsolidado(
+      refAlimentacaoCEI,
+      refDietaACEI,
+      refDietaBCEI,
+    );
+
+    const emei = calcularConsolidado(
+      refAlimentacaoEMEI,
+      refDietaAEMEI,
+      refDietaBEMEI,
+    );
+
+    setConsolidadoCEI(cei);
+    setConsolidadoEMEI(emei);
+  }, []);
+
+  const cards = useMemo(() => {
+    return [
+      {
+        titulo: "CONSOLIDADO INFANTIL (INF. A + INF. B + INF. C)",
+        quantidade: consolidadoInfantil.quantidade,
+        tituloQuantidade: "QUANTIDADE SERVIDA (INF. A + INF. B + INF. C):",
+        valor: consolidadoInfantil.valor,
+        tituloValor: "VALOR DO FATURAMENTO TOTAL (INF. A + INF. B + INF. C):",
+      },
+      {
+        titulo: "CONSOLIDADO FUNDAMENTAL (FUND. A + FUND. B + FUND. C)",
+        quantidade: consolidadoFundamental.quantidade,
+        tituloQuantidade: "QUANTIDADE SERVIDA (FUND. A + FUND. B + FUND. C):",
+        valor: consolidadoFundamental.valor,
+        tituloValor:
+          "VALOR DO FATURAMENTO TOTAL (FUND. A + FUND. B + FUND. C):",
+      },
+      {
+        titulo:
+          "CONSOLIDADO TOTAL (INF. A + INF. B + INF. C + FUND. A + FUND. B + FUND. C)",
+        quantidade:
+          consolidadoFundamental.quantidade + consolidadoInfantil.quantidade,
+        tituloQuantidade: "QUANTIDADE SERVIDA:",
+        valor: consolidadoFundamental.valor + consolidadoInfantil.valor,
+        tituloValor: "VALOR DO FATURAMENTO TOTAL:",
+      },
+    ];
+  }, [consolidadoInfantil, consolidadoFundamental]);
+
+  return (
+    <div>
+      {cards.map((card, index) => (
+        <div className="mt-4" key={index}>
+          <ConsolidadoTotal
+            titulo={card.titulo}
+            quantidade={card.quantidade}
+            tituloQuantidade={card.tituloQuantidade}
+            valor={card.valor}
+            tituloValor={card.tituloValor}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
