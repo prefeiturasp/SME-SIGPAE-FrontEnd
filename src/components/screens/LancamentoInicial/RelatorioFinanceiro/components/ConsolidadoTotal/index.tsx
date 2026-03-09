559a6646e7064897b0c6fa8d3c25b18b72cd574a
GuiLMassini <guimassinifai@gmail.com>
2026-03-09 09:32:43 -0300

    feat(140464): adaptado componente para renderizacao dinamica

--- src/components/screens/LancamentoInicial/RelatorioFinanceiro/components/ConsolidadoTotal/index.tsx
+++ src/components/screens/LancamentoInicial/RelatorioFinanceiro/components/ConsolidadoTotal/index.tsx
@@ -5,26 +5,32 @@ import { capitalize } from "src/helpers/utilities";
 
 type Props = {
   titulo: string;
+  tituloQuantidade?: string;
   quantidade: number;
+  tituloValor?: string;
   valor: number;
 };
 
-export function ConsolidadoTotal({ titulo, quantidade, valor }: Props) {
+export function ConsolidadoTotal({
+  titulo,
+  tituloQuantidade = "QUANTIDADE SERVIDA (A + B + C):",
+  quantidade,
+  tituloValor = "VALOR DO FATURAMENTO TOTAL (A + B + C):",
+  valor,
+}: Props) {
   return (
     <div className="consolidado-total">
       <h2 className="consolidado-titulo">{titulo}</h2>
       <div className="consolidado-conteudo">
         <div className="bloco">
-          <span className="bloco-titulo">QUANTIDADE SERVIDA (A + B + C):</span>
+          <span className="bloco-titulo">{tituloQuantidade}</span>
           <div className="caixa quantidade">
             <span className="valor">{formataMilhar(quantidade)}</span>
             <span className="unidade">ALIMENTAÇÕES</span>
           </div>
         </div>
         <div className="bloco bloco-grande">
-          <span className="bloco-titulo">
-            VALOR DO FATURAMENTO TOTAL (A + B + C):
-          </span>
+          <span className="bloco-titulo">{tituloValor}</span>
           <div className="caixa valor">
             <span className="valor">R$ {formataMilharDecimal(valor)}</span>
             <span className="extenso">
