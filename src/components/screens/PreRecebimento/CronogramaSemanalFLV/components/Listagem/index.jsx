f781f971dabc5a68268694ca19c90163ee08f19c
Murilo Ferri Schirmer <murilo.schirmer@spassu.com.br>
2026-04-16 12:27:47 -0300

    feat(140181): :sparkles: implementa filtros e listagem de cronogramas semanais

---
+++ src/components/screens/PreRecebimento/CronogramaSemanalFLV/components/Listagem/index.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+import "./styles.scss";
+import {
+  usuarioEhEmpresaFornecedor,
+  formataMilhar,
+} from "src/helpers/utilities";
+import { Tooltip } from "antd";
+import { formataNome } from "./helpers";
+
+const ListagemCronogramas = ({ cronogramas, ativos }) => {
+  const statusValue = (status) => {
+    if (
+      status === "Assinado e Enviado ao Fornecedor" &&
+      usuarioEhEmpresaFornecedor()
+    ) {
+      return "Recebido";
+    } else {
+      return status;
+    }
+  };
+
+  return (
+    <section className="resultado-cronograma-semanal mt-5">
+      <header>
+        <div className="row">
+          <div className="col-5">
+            <p className="titulo-grid-alunos-matriculados">
+              Resultados da Pesquisa
+            </p>
+          </div>
+        </div>
+      </header>
+      <article>
+        <div className="grid-table header-table">
+          <div>N° do Cronograma Ponto a Ponto</div>
+          <div>Nome do Produto</div>
+          <div>Quantidade</div>
+          <div>Fornecedor</div>
+          <div>Status</div>
+          <div>Ações</div>
+        </div>
+        {cronogramas.map((cronograma, index) => {
+          const bordas =
+            ativos && ativos.includes(cronograma.uuid) ? "desativar-borda" : "";
+          return (
+            <div key={`${cronograma.numero}_${index}`}>
+              <div className="grid-table body-table">
+                <div className={bordas}>{cronograma.numero}</div>
+                <div className="d-flex align-items-center justify-content-between">
+                  <Tooltip
+                    color="#42474a"
+                    overlayStyle={{
+                      maxWidth: "320px",
+                      fontSize: "12px",
+                      fontWeight: "700",
+                    }}
+                    title={cronograma.produto}
+                  >
+                    {cronograma.produto && formataNome(cronograma.produto)}
+                  </Tooltip>
+                </div>
+                <div className={bordas}>
+                  {cronograma.quantidade_total &&
+                    formataMilhar(cronograma.quantidade_total)}{" "}
+                  {cronograma.unidade_medida}
+                </div>
+                <div className={bordas}>
+                  {cronograma.empresa ? cronograma.empresa : undefined}
+                </div>
+                <div className={bordas}>{statusValue(cronograma.status)}</div>
+
+                <div className={bordas}></div>
+              </div>
+            </div>
+          );
+        })}
+      </article>
+    </section>
+  );
+};
+
+export default ListagemCronogramas;
