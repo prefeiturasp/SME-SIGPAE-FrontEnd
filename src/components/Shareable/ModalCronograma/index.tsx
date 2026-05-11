11060d9adde89316ddd846b80f7290c2ff1b99b2
Marcus Martins <marcusmartins38@gmail.com>
2026-05-11 15:10:49 -0400

    feat(140786): modal de cronograma

---
+++ src/components/Shareable/ModalCronograma/index.tsx
@@ -0,0 +1,144 @@
+import React from "react";
+import { Modal } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import Botao from "src/components/Shareable/Botao";
+import {
+  BUTTON_STYLE,
+  BUTTON_TYPE,
+} from "src/components/Shareable/Botao/constants";
+import TagLeveLeite from "src/components/Shareable/PreRecebimento/TagLeveLeite";
+import {
+  EtapaCalendario,
+  ItemCalendario,
+} from "src/interfaces/pre_recebimento.interface";
+import {
+  ALTERACAO_CRONOGRAMA,
+  CRONOGRAMA_ENTREGA,
+  PRE_RECEBIMENTO,
+} from "../../../configs/constants";
+import {
+  usuarioEhCodaeDilog,
+  usuarioEhCronograma,
+  usuarioEhDilogDiretoria,
+} from "../../../helpers/utilities";
+import "./style.scss";
+
+interface Props {
+  event: ItemCalendario<EtapaCalendario>;
+  showModal: boolean;
+  closeModal: () => void;
+  ehCronogramaPontoAPonto?: boolean;
+}
+
+export const ModalCronograma: React.FC<Props> = ({
+  event,
+  showModal,
+  closeModal,
+  ehCronogramaPontoAPonto = false,
+}) => {
+  const navigate = useNavigate();
+
+  return (
+    <Modal
+      dialogClassName="modal-info-cronograma modal-50w"
+      show={showModal}
+      onHide={closeModal}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Informações do Cronograma</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="row">
+          <div className="col-12">
+            <span className="fw-bold">Nº do Cronograma: </span>
+            <span className="fw-bold green">
+              {event.objeto.numero_cronograma}
+            </span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <span className="fw-bold">Nome do Produto: </span>
+            <span>{event.objeto.nome_produto}</span>
+            {event.programa_leve_leite && (
+              <TagLeveLeite className="ms-3 absolute" />
+            )}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <span className="fw-bold">Nome do Fornecedor: </span>
+            <span>{event.objeto.nome_fornecedor}</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <span className="fw-bold">Data da Entrega: </span>
+            <span>{event.objeto.data_programada}</span>
+          </div>
+          <div className="col-6">
+            <span className="fw-bold">Empenho: </span>
+            <span>{event.objeto.numero_empenho}</span>
+          </div>
+        </div>
+        <div className="row">
+          {!ehCronogramaPontoAPonto && (
+            <div className="col-4">
+              <span className="fw-bold">Etapa: </span>
+              <span>{event.objeto.etapa}</span>
+            </div>
+          )}
+          {!ehCronogramaPontoAPonto && (
+            <div className="col-4">
+              <span className="fw-bold">Parte: </span>
+              <span>{event.objeto.parte}</span>
+            </div>
+          )}
+          <div className="col-4">
+            <span className="fw-bold">Quantidade: </span>
+            <span>
+              {event.objeto.quantidade} {event.objeto.unidade_medida}
+            </span>
+          </div>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        {ehCronogramaPontoAPonto
+          ? (usuarioEhCronograma() || usuarioEhCodaeDilog()) && (
+              <Botao
+                texto="Alterar"
+                type={BUTTON_TYPE.BUTTON}
+                onClick={() => {
+                  navigate(`/${PRE_RECEBIMENTO}/${CRONOGRAMA_ENTREGA}`);
+                }}
+                style={BUTTON_STYLE.GREEN_OUTLINE}
+                className="ms-3"
+              />
+            )
+          : (usuarioEhCronograma() ||
+              usuarioEhDilogDiretoria() ||
+              usuarioEhCodaeDilog()) &&
+            event.objeto.status === "Assinado CODAE" && (
+              <Botao
+                texto="Alterar"
+                type={BUTTON_TYPE.BUTTON}
+                onClick={() => {
+                  navigate(
+                    `/${PRE_RECEBIMENTO}/${ALTERACAO_CRONOGRAMA}?uuid=${event.objeto.uuid_cronograma}`,
+                  );
+                }}
+                style={BUTTON_STYLE.GREEN_OUTLINE}
+                className="ms-3"
+              />
+            )}
+        <Botao
+          texto="Fechar"
+          onClick={closeModal}
+          type={BUTTON_TYPE.BUTTON}
+          style={BUTTON_STYLE.GREEN}
+          className="ms-3"
+        />
+      </Modal.Footer>
+    </Modal>
+  );
+};
