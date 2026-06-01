4dd497974e01cbfdf32a23d22e8ae5e83a28aeb7
Priscyla C. dos Santos <santospriscyla@gmail.com>
2026-06-01 16:29:46 -0300

    test(148654): Inclui mock e testes iniciais para  recreio de unidades CEI

---
+++ src/components/screens/LancamentoInicial/PeriodoLancamentoMedicaoInicialCEI/__tests__/CEI/RecreioNasFerias/regra_liberacao_apontamentos.test.jsx
@@ -0,0 +1,302 @@
+import "@testing-library/jest-dom";
+import { act, render, screen, waitFor } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+import { PeriodoLancamentoMedicaoInicialCEI } from "src/components/screens/LancamentoInicial/PeriodoLancamentoMedicaoInicialCEI";
+import { mockFaixasEtarias } from "src/mocks/faixaEtaria.service/mockGetFaixasEtarias";
+import { mockCategoriasMedicaoCEI } from "src/mocks/medicaoInicial/PeriodoLancamentoMedicaoInicialCEI/mockCategoriasMedicaoCEI";
+import { mockSalvaLancamentoSemana1 } from "src/mocks/medicaoInicial/PeriodoLancamentoMedicaoInicialCEI/RecreioNasFerias/CEI/mockSalvarLancamentos";
+import { mockValoresMedicaoCEI } from "src/mocks/medicaoInicial/PeriodoLancamentoMedicaoInicialCEI/RecreioNasFerias/CEI/mockValoresMedicaoCEI";
+import { mockDiasLetivosRecreio } from "src/mocks/medicaoInicial/PeriodoLancamentoMedicaoInicialCEI/RecreioNasFerias/CEI/diasLetivosRecreio.jsx";
+import { mockMeusDadosEscolaCEI } from "src/mocks/medicaoInicial/PeriodoLancamentoMedicaoInicialCEI/mockMeusDadosEscolaCEI";
+import { getTiposDeAlimentacao } from "src/services/cadastroTipoAlimentacao.service";
+import { getListaDiasSobremesaDoce } from "src/services/medicaoInicial/diaSobremesaDoce.service";
+import { mockLocationStateGrupoRecreioNasFerias } from "src/mocks/medicaoInicial/PeriodoLancamentoMedicaoInicialCEI/RecreioNasFerias/CEI/mockStateRecreio";
+import { mockDietasEspsciais } from "src/mocks/medicaoInicial/PeriodoLancamentoMedicaoInicialCEI/RecreioNasFerias/CEI/mockDietasEspeciais.jsx";
+import {
+  getCategoriasDeMedicao,
+  getDiasLetivosRecreio,
+  getDiasParaCorrecao,
+  getFeriadosNoMes,
+  getSolicitacoesAlteracoesAlimentacaoAutorizadasEscola,
+  getSolicitacoesInclusoesAutorizadasEscola,
+  getSolicitacoesSuspensoesAutorizadasEscola,
+  getValoresPeriodosLancamentos,
+  updateValoresPeriodosLancamentos,
+  getLogDietasAutorizadasRecreioNasFeriasCEI,
+} from "src/services/medicaoInicial/periodoLancamentoMedicao.service";
+import { getMeusDados } from "src/services/perfil.service";
+import mock from "src/services/_mock";
+import { localStorageMock } from "src/mocks/localStorageMock";
+import preview from "jest-preview";
+jest.mock("src/services/perfil.service.jsx");
+jest.mock("src/services/medicaoInicial/diaSobremesaDoce.service.jsx");
+jest.mock("src/services/cadastroTipoAlimentacao.service");
+jest.mock("src/services/medicaoInicial/periodoLancamentoMedicao.service");
+
+const awaitServices = async () => {
+  await waitFor(() => {
+    expect(getListaDiasSobremesaDoce).toHaveBeenCalled();
+    expect(getSolicitacoesInclusoesAutorizadasEscola).toHaveBeenCalled();
+    expect(getCategoriasDeMedicao).toHaveBeenCalled();
+    expect(getDiasParaCorrecao).toHaveBeenCalled();
+    expect(getValoresPeriodosLancamentos).toHaveBeenCalled();
+    expect(getSolicitacoesSuspensoesAutorizadasEscola).toHaveBeenCalled();
+    expect(
+      getSolicitacoesAlteracoesAlimentacaoAutorizadasEscola,
+    ).toHaveBeenCalled();
+    expect(getDiasLetivosRecreio).toHaveBeenCalled();
+    expect(getFeriadosNoMes).toHaveBeenCalled();
+    expect(getLogDietasAutorizadasRecreioNasFeriasCEI).toHaveBeenCalled;
+  });
+};
+
+describe("Teste Grupo Recreio Nas Férias OUTUBRO/2025 - EMEF: Regra de liberação dos dias para apontamento", () => {
+  beforeEach(async () => {
+    jest.useFakeTimers();
+    jest.setSystemTime(new Date("2025-10-03T10:00:00"));
+    getMeusDados.mockResolvedValue({
+      data: mockMeusDadosEscolaCEI,
+      status: 200,
+    });
+    getListaDiasSobremesaDoce.mockResolvedValue({ data: [], status: 200 });
+    getSolicitacoesInclusoesAutorizadasEscola.mockResolvedValue({
+      data: { results: [] },
+      status: 200,
+    });
+    getCategoriasDeMedicao.mockResolvedValue({
+      data: mockCategoriasMedicaoCEI,
+      status: 200,
+    });
+
+    getTiposDeAlimentacao.mockResolvedValue({
+      data: { data: { results: [] }, status: 200 },
+      status: 200,
+    });
+    getValoresPeriodosLancamentos.mockResolvedValue({
+      data: mockValoresMedicaoCEI,
+      status: 200,
+    });
+    getDiasParaCorrecao.mockResolvedValue({
+      data: [],
+      status: 200,
+    });
+    getSolicitacoesSuspensoesAutorizadasEscola.mockResolvedValue({
+      data: { data: { results: [] }, status: 200 },
+      status: 200,
+    });
+
+    getSolicitacoesAlteracoesAlimentacaoAutorizadasEscola.mockResolvedValue({
+      data: { results: [] },
+      status: 200,
+    });
+    getDiasLetivosRecreio.mockResolvedValue({
+      data: mockDiasLetivosRecreio,
+      status: 200,
+    });
+    getFeriadosNoMes.mockResolvedValue({
+      data: { results: ["12"] },
+      status: 200,
+    });
+    updateValoresPeriodosLancamentos.mockResolvedValue({
+      data: mockSalvaLancamentoSemana1,
+      status: 200,
+    });
+    mock.onGet("/faixas-etarias/").reply(200, mockFaixasEtarias);
+    getLogDietasAutorizadasRecreioNasFeriasCEI.mockResolvedValue({
+      data: mockDietasEspsciais,
+      status: 200,
+    });
+
+    Object.defineProperty(global, "localStorage", { value: localStorageMock });
+    localStorage.setItem("eh_cemei", "false");
+
+    await act(async () => {
+      render(
+        <MemoryRouter
+          initialEntries={[
+            { pathname: "/", state: mockLocationStateGrupoRecreioNasFerias },
+          ]}
+          future={{
+            v7_startTransition: true,
+            v7_relativeSplatPath: true,
+          }}
+        >
+          <PeriodoLancamentoMedicaoInicialCEI />
+          <ToastContainer />
+        </MemoryRouter>,
+      );
+    });
+  });
+
+  describe("Testa conteúdo básico da tela", () => {
+    it("consulta os valores do recreio com o grupo simples de CEI", async () => {
+      await awaitServices();
+
+      expect(getValoresPeriodosLancamentos).toHaveBeenCalledWith(
+        expect.objectContaining({
+          nome_grupo: "Recreio nas Férias",
+        }),
+      );
+      expect(getDiasParaCorrecao).toHaveBeenCalledWith(
+        expect.objectContaining({
+          nome_grupo: "Recreio nas Férias",
+        }),
+      );
+    });
+
+    it("renderiza label `Mês do Lançamento`", async () => {
+      await awaitServices();
+      expect(screen.getByText("Mês do Lançamento")).toBeInTheDocument();
+    });
+
+    it("renderiza valor `Recreio nas Férias - DEZ 2025` Mês do Lançamento`", () => {
+      const inputElement = screen.getByTestId("input-mes-lancamento");
+      expect(inputElement).toHaveAttribute(
+        "value",
+        mockLocationStateGrupoRecreioNasFerias.solicitacaoMedicaoInicial
+          .recreio_nas_ferias.titulo,
+      );
+    });
+
+    it("renderiza label `Tipo de Lançamento`", () => {
+      expect(screen.getByText("Tipo de Lançamento")).toBeInTheDocument();
+    });
+
+    it("renderiza valor `Recreio nas Férias` no input `Tipo de Lançamento`", () => {
+      const inputElement = screen.getByTestId("input-periodo-lancamento");
+      expect(inputElement).toHaveAttribute(
+        "value",
+        mockLocationStateGrupoRecreioNasFerias.periodo,
+      );
+    });
+
+    it("renderiza quadro de legendas", () => {
+      expect(screen.getByText("Legenda das Informações:")).toBeInTheDocument();
+      expect(
+        screen.getByText(
+          "Há erros no lançamento. Corrija para conseguir salvar.",
+        ),
+      ).toBeInTheDocument();
+      expect(
+        screen.getByText(
+          "Há divergências no lançamento. Adicione uma observação.",
+        ),
+      ).toBeInTheDocument();
+      expect(
+        screen.getByText(
+          "Atenção! Verifique se está correto e prossiga os apontamentos.",
+        ),
+      ).toBeInTheDocument();
+    });
+
+    it("renderiza label `Semanas do Período para Lançamento da Medição Inicial`", () => {
+      expect(
+        screen.getByText(
+          "Semanas do Período para Lançamento da Medição Inicial",
+        ),
+      ).toBeInTheDocument();
+    });
+
+    it("renderiza as labels `Semana 1` e `Semana 2` e `Semana 3`", async () => {
+      await awaitServices();
+      expect(screen.getByText("Semana 1")).toBeInTheDocument();
+      expect(screen.getByText("Semana 2")).toBeInTheDocument();
+    });
+
+    it("não renderiza as labels  `Semana 4`, `Semana 5`", async () => {
+      await awaitServices();
+      expect(screen.queryByText("Semana 5")).not.toBeInTheDocument();
+    });
+
+    it("renderiza label `ALIMENTAÇÃO`", async () => {
+      await awaitServices();
+      expect(screen.getByText("ALIMENTAÇÃO")).toBeInTheDocument();
+    });
+
+    it("renderiza label `Participantes` dentro da seção `ALIMENTAÇÃO`", async () => {
+      await awaitServices();
+      const categoriaAlimentacaoUuid = "0e1f14ce-685a-4d4c-b0a7-96efe52b754f";
+      const myElement = screen.getByTestId(
+        `div-lancamentos-por-categoria-${categoriaAlimentacaoUuid}`,
+      );
+      const allParticipantes = screen.getAllByText("Participantes");
+      const specificParticipantes = allParticipantes.find((element) =>
+        myElement.contains(element),
+      );
+      expect(specificParticipantes).toBeInTheDocument();
+    });
+
+    it("renderiza label `Seg.` dentro da seção `ALIMENTAÇÃO`", async () => {
+      await awaitServices();
+      const categoriaAlimentacaoUuid = "0e1f14ce-685a-4d4c-b0a7-96efe52b754f";
+      const myElement = screen.getByTestId(
+        `div-lancamentos-por-categoria-${categoriaAlimentacaoUuid}`,
+      );
+      const allParticipantes = screen.getAllByText("Seg.");
+      const specificParticipantes = allParticipantes.find((element) =>
+        myElement.contains(element),
+      );
+      expect(specificParticipantes).toBeInTheDocument();
+    });
+
+    it("renderiza label `DIETA ESPECIAL - TIPO A`", async () => {
+      await awaitServices();
+      expect(screen.getByText("DIETA ESPECIAL - TIPO A")).toBeInTheDocument();
+    });
+
+    it("renderiza label `Dietas Autorizadas` dentro da seção `DIETA ESPECIAL - TIPO A`", async () => {
+      await awaitServices();
+      const categoriaDietaAUuid = "39cd2574-6d28-49bb-b628-ae538dc97791";
+      const myElement = screen.getByTestId(
+        `div-lancamentos-por-categoria-${categoriaDietaAUuid}`,
+      );
+      const dietasAutorizadas = screen.getAllByText("Dietas Autorizadas");
+      const specificParticipantes = dietasAutorizadas.find((element) =>
+        myElement.contains(element),
+      );
+      expect(specificParticipantes).toBeInTheDocument();
+    });
+
+    it("renderiza label `Seg.` dentro da seção `DIETA ESPECIAL - TIPO A`", async () => {
+      await awaitServices();
+      const categoriaDietaAUuid = "39cd2574-6d28-49bb-b628-ae538dc97791";
+      const myElement = screen.getByTestId(
+        `div-lancamentos-por-categoria-${categoriaDietaAUuid}`,
+      );
+      const dietasAutorizadas = screen.getAllByText("Seg.");
+      const specificParticipantes = dietasAutorizadas.find((element) =>
+        myElement.contains(element),
+      );
+      expect(specificParticipantes).toBeInTheDocument();
+    });
+
+    it("renderiza label `Dietas Autorizadas` dentro da seção `DIETA ESPECIAL - TIPO B`", async () => {
+      await awaitServices();
+      const categoriaDietaAUuid = "6ad79709-3611-4af3-a567-65fcf34b3d06";
+      const myElement = screen.getByTestId(
+        `div-lancamentos-por-categoria-${categoriaDietaAUuid}`,
+      );
+      const dietasAutorizadas = screen.getAllByText("Dietas Autorizadas");
+      const specificParticipantes = dietasAutorizadas.find((element) =>
+        myElement.contains(element),
+      );
+      expect(specificParticipantes).toBeInTheDocument();
+    });
+
+    it("renderiza label `Seg.` dentro da seção `DIETA ESPECIAL - TIPO B`", async () => {
+      await awaitServices();
+      const categoriaDietaAUuid = "6ad79709-3611-4af3-a567-65fcf34b3d06";
+      const myElement = screen.getByTestId(
+        `div-lancamentos-por-categoria-${categoriaDietaAUuid}`,
+      );
+      const dietasAutorizadas = screen.getAllByText("Seg.");
+      const specificParticipantes = dietasAutorizadas.find((element) =>
+        myElement.contains(element),
+      );
+      expect(specificParticipantes).toBeInTheDocument();
+      preview.debug();
+    });
+  });
+});
