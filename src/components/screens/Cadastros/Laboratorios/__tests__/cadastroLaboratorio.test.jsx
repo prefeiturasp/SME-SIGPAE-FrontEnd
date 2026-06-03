5c960acbedba842ca1e0e3dced41684d6d30b756
Priscyla C. dos Santos <santospriscyla@gmail.com>
2026-06-03 12:32:30 -0300

    test(149738): Testas os textos iniciais

--- src/components/screens/Cadastros/Laboratorios/__tests__/cadastroLaboratorio.test.jsx
+++ src/components/screens/Cadastros/Laboratorios/__tests__/cadastroLaboratorio.test.jsx
@@ -16,11 +16,11 @@ describe("Testa o componente CadastroLaboratorio", () => {
       data: {
         results: [
           {
-            nome: "TESTE 2",
+            nome: "Laboratorio São Paulo",
             cnpj: "50708401000179",
           },
           {
-            nome: "TESTE CADASTRO 123",
+            nome: "Laboratorio Rio de Janeiro",
             cnpj: "50708401000179",
           },
         ],
@@ -44,5 +44,45 @@ describe("Testa o componente CadastroLaboratorio", () => {
     expect(screen.getByText("Endereço do Laboratório")).toBeInTheDocument();
 
     expect(screen.getByText("Contatos")).toBeInTheDocument();
+
+    expect(screen.getByText("Laboratórios Credenciados")).toBeInTheDocument();
+  });
+
+  it("deve exibir os dados da secção Dados do Laboratório", async () => {
+    expect(screen.getByText("Nome do Laboratório")).toBeInTheDocument();
+
+    expect(screen.getByText("CNPJ do Laboratório")).toBeInTheDocument();
+
+    expect(screen.getByText("Data de Cadastro")).toBeInTheDocument();
+  });
+
+  it("deve exibir os dados da secção Endereço do Laboratório", async () => {
+    expect(screen.getByText("CEP")).toBeInTheDocument();
+
+    expect(screen.getByText("Logradouro")).toBeInTheDocument();
+
+    expect(screen.getByText("Número")).toBeInTheDocument();
+    expect(screen.getByText("Complemento")).toBeInTheDocument();
+
+    expect(screen.getByText("Bairro")).toBeInTheDocument();
+
+    expect(screen.getByText("Cidade")).toBeInTheDocument();
+    expect(screen.getByText("Estado")).toBeInTheDocument();
+  });
+
+  it("deve exibir os dados da secção Contatos", async () => {
+    expect(screen.getByText("Nome")).toBeInTheDocument();
+
+    expect(screen.getByText("Telefone")).toBeInTheDocument();
+
+    expect(screen.getByText("E-mail")).toBeInTheDocument();
+  });
+
+  it("deve exibir os dados da secção Laboratórios Credenciados", async () => {
+    expect(
+      screen.getByText("Esse Laboratório está Credenciado?"),
+    ).toBeInTheDocument();
+    expect(screen.getByText("SIM")).toBeInTheDocument();
+    expect(screen.getByText("NÃO")).toBeInTheDocument();
   });
 });
