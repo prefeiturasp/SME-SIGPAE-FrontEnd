8de950332267271637cc5a5b2279388cc6634b35
Calvin Rossinhole <calvin.masters@gmail.com>
2026-06-18 15:38:50 -0300

    feat(148324): :sparkles: filtro por tipos unidades e ues

--- src/components/screens/Cadastros/DiasLetivosSIGPAE/Editar/index.tsx
+++ src/components/screens/Cadastros/DiasLetivosSIGPAE/Editar/index.tsx
@@ -5,9 +5,15 @@ import { lotesToOptions } from "src/components/screens/Relatorios/SolicitacoesAl
 import { MultiselectRaw } from "src/components/Shareable/MultiselectRaw";
 import { requiredMultiselect } from "src/helpers/fieldValidators";
 import { getLotesSimples } from "src/services/lote.service";
+import { getUnidadesEducacionaisComCodEol } from "src/services/dietaEspecial.service";
+import { toastError } from "src/components/Shareable/Toast/dialogs";
+import { getTiposUnidadeEscolar } from "src/services/cadastroTipoAlimentacao.service";
+import { Spin } from "antd";
 
 export const EditarDiasLetivosSIGPAE = () => {
   const [lotes, setLotes] = useState([]);
+  const [tiposUnidades, setTiposUnidades] = useState([]);
+  const [unidadesEducacionais, setUnidadesEducacionais] = useState([]);
 
   const [erroAPI, setErroAPI] = useState("");
 
@@ -16,14 +22,53 @@ export const EditarDiasLetivosSIGPAE = () => {
     if (response.status === HTTP_STATUS.OK) {
       setLotes(lotesToOptions(response.data.results));
     } else {
-      setErroAPI("Erro ao carregar lotes.");
+      setErroAPI("Erro ao carregar lotes. Tente novamente mais tarde.");
+    }
+  };
+
+  const getTiposUnidadesUEAsync = async () => {
+    const response = await getTiposUnidadeEscolar({
+      pertence_relatorio_solicitacoes_alimentacao: true,
+    });
+    if (response.status === HTTP_STATUS.OK) {
+      setTiposUnidades(response.data.results);
+    } else {
+      setErroAPI(
+        "Erro ao carregar tipos de unidade educacional. Tente novamente mais tarde.",
+      );
+    }
+  };
+
+  const getUnidadesEducacionaisAsync = async (values) => {
+    setUnidadesEducacionais([]);
+    let data = values;
+    const response = await getUnidadesEducacionaisComCodEol(data);
+    if (response.status === HTTP_STATUS.OK) {
+      if (response.data.mensagem) {
+        setUnidadesEducacionais([
+          {
+            label: response.data.mensagem,
+            value: "__no_result__",
+            disabled: true,
+          },
+        ]);
+        return;
+      }
+      setUnidadesEducacionais(
+        response.data.map((unidade) => ({
+          label: `${unidade.codigo_eol_escola}`,
+          value: unidade.uuid,
+        })),
+      );
+    } else {
+      toastError("Erro ao buscar unidades educacionais");
     }
   };
 
   const onSubmit = () => {};
 
   useEffect(() => {
-    Promise.all([getLotesSimplesAsync()]);
+    Promise.all([getLotesSimplesAsync(), getTiposUnidadesUEAsync()]);
   }, []);
 
   return (
@@ -45,17 +90,77 @@ export const EditarDiasLetivosSIGPAE = () => {
                         name="lotes"
                         selected={values.lotes || []}
                         options={lotes}
-                        onSelectedChanged={(values_) =>
+                        onSelectedChanged={(values_) => {
                           form.change(
                             `lotes`,
                             values_.map((value_) => value_.value),
-                          )
-                        }
+                          );
+                          form.change(`tipos_unidades`, undefined);
+                        }}
                         hasSelectAll
-                        placeholder="Selecione Lote(s)"
+                        placeholder="Selecione os Lote(s)"
                         required
                         validate={requiredMultiselect}
                       />
+                    </div>
+                    <div className="col-4">
+                      <label className="label fw-normal pb-2 pt-2">
+                        <span className="red">*</span> Tipo de Unidade
+                      </label>
+                      <Field
+                        component={MultiselectRaw}
+                        placeholder="Selecione o(s) tipo(s) de unidade"
+                        name="tipos_unidades"
+                        dataTestId="select-tipos-unidades"
+                        options={tiposUnidades.map((tipo_unidade) => ({
+                          label: tipo_unidade.iniciais,
+                          value: tipo_unidade.uuid,
+                        }))}
+                        selected={values.tipos_unidades || []}
+                        onSelectedChanged={async (values_) => {
+                          form.change(
+                            `tipos_unidades`,
+                            values_.map((value_) => value_.value),
+                          );
+                          await getUnidadesEducacionaisAsync({
+                            lotes: values.lotes,
+                            tipos_unidades: values_.map(
+                              (value_) => value_.value,
+                            ),
+                          });
+                        }}
+                        required
+                        validate={requiredMultiselect}
+                      />
+                    </div>
+                    <div className="col-4">
+                      <label className="label fw-normal pb-2 pt-2">
+                        Unidades Educacionais
+                      </label>
+                      <Spin
+                        tip="Carregando unidades educacionais..."
+                        spinning={
+                          values.lotes !== undefined &&
+                          values.lotes.length > 0 &&
+                          values.tipos_unidades !== undefined &&
+                          values.tipos_unidades.length > 0 &&
+                          unidadesEducacionais.length === 0
+                        }
+                      >
+                        <Field
+                          component={MultiselectRaw}
+                          name="unidades_educacionais"
+                          options={unidadesEducacionais}
+                          selected={values.unidades_educacionais || []}
+                          onSelectedChanged={(values_) => {
+                            form.change(
+                              "unidades_educacionais",
+                              values_.map((value_) => value_.value),
+                            );
+                          }}
+                          disabled={!values.lotes && !values.tipos_unidades}
+                        />
+                      </Spin>
                     </div>
                   </div>
                 </form>
