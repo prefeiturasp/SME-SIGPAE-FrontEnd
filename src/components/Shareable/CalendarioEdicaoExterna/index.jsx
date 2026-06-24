972af3162c7ede4a90d3ce36351f480ec92d68c0
Calvin Rossinhole <calvin.masters@gmail.com>
2026-06-24 14:41:02 -0300

    refactor(148323): :hammer: converte calendario para function component

--- src/components/Shareable/CalendarioEdicaoExterna/index.jsx
+++ src/components/Shareable/CalendarioEdicaoExterna/index.jsx
@@ -2,7 +2,7 @@ import { Spin } from "antd";
 import HTTP_STATUS from "http-status-codes";
 import moment from "moment";
 import "moment/dist/locale/pt-br";
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Calendar, momentLocalizer, Views } from "react-big-calendar";
 import dragAndDropAddon from "react-big-calendar/lib/addons/dragAndDrop";
 import { ModalDadosObjeto } from "src/components/screens/Cadastros/DiasLetivosSIGPAE/components/ModalDadosObjeto";
@@ -16,137 +16,111 @@ const DragAndDropCalendar = withDragAndDrop(Calendar);
 
 const localizer = momentLocalizer(moment);
 
-export class CalendarioEdicaoExterna extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      objetos: undefined,
-      loadingDiasCalendario: false,
-      erroAPI: false,
-      currentEvent: undefined,
-      showModalDadosObjeto: false,
-      mes: moment().month() + 1,
-      ano: moment().year(),
-      hasNavigatedOnce: false,
-    };
+export const CalendarioEdicaoExterna = ({
+  getObjetos,
+  nomeObjeto,
+  nomeObjetoMinusculo,
+  setObjeto,
+}) => {
+  const [objetos, setObjetos] = useState();
+  const [loadingDiasCalendario, setLoadingDiasCalendario] = useState(false);
+  const [erroAPI] = useState(false);
+  const [currentEvent, setCurrentEvent] = useState();
+  const [showModalDadosObjeto, setShowModalDadosObjeto] = useState(false);
+  const [hasNavigatedOnce, setHasNavigatedOnce] = useState(false);
 
-    this.handleEvent = this.handleEvent.bind(this);
-    this.getObjetosAsync = this.getObjetosAsync.bind(this);
-  }
+  const [mes, setMes] = useState(moment().month() + 1);
+  const [ano, setAno] = useState(moment().year());
 
-  componentDidMount() {
-    this.getObjetosAsync();
-  }
+  const getObjetosAsync = useCallback(
+    async (params) => {
+      setLoadingDiasCalendario(true);
+      const response = await getObjetos(
+        params ? { mes: params.mes, ano: params.ano } : { mes, ano },
+      );
+      if (response.status === HTTP_STATUS.OK) {
+        setObjetos(formataComoEventos(response.data.results || response.data));
+      }
+      if (response) {
+        setLoadingDiasCalendario(false);
+      }
+    },
+    [getObjetos, mes, ano],
+  );
 
-  async getObjetosAsync(params) {
-    const { getObjetos } = this.props;
-    const { mes, ano } = this.state;
-    this.setState({ loadingDiasCalendario: true });
-    const response = await getObjetos(
-      params ? { mes: params.mes, ano: params.ano } : { mes, ano },
-    );
-    if (response.status === HTTP_STATUS.OK) {
-      this.setState({
-        objetos: formataComoEventos(response.data.results || response.data),
-      });
-    }
-    if (response) {
-      this.setState({ loadingDiasCalendario: false });
-    }
-  }
+  useEffect(() => {
+    getObjetosAsync();
+  }, []);
 
-  handleEvent(event) {
-    this.setState({
-      currentEvent: event,
-      showModalDadosObjeto: true,
-    });
-  }
+  const handleEvent = useCallback((event) => {
+    setCurrentEvent(event);
+    setShowModalDadosObjeto(true);
+  }, []);
 
-  render() {
-    const {
-      objetos,
-      loadingDiasCalendario,
-      erroAPI,
-      hasNavigatedOnce,
-      currentEvent,
-      showModalDadosObjeto,
-    } = this.state;
-
-    const { nomeObjeto, nomeObjetoMinusculo, setObjeto } = this.props;
-
-    return (
-      <div className="card calendario-sobremesa mt-3">
-        <div className="card-body">
-          <Spin tip="Carregando calendário..." spinning={!objetos && !erroAPI}>
-            {erroAPI && <div>{erroAPI}</div>}
-            {objetos && (
-              <>
-                <Spin
-                  tip={`Carregando dias ${nomeObjeto}...`}
-                  spinning={loadingDiasCalendario}
-                >
-                  <DragAndDropCalendar
-                    tooltipAccessor={(e) => e.editais_numeros}
-                    style={{ height: 1000 }}
-                    formats={{
-                      weekdayFormat: (date, culture, localizer) =>
-                        localizer.format(date, "dddd", culture),
-                    }}
-                    selectable
-                    resizable={false}
-                    localizer={localizer}
-                    events={objetos}
-                    onSelectEvent={this.handleEvent}
-                    components={{
-                      toolbar: CustomToolbar,
-                    }}
-                    messages={{
-                      showMore: (target) => (
-                        <span className="ms-2" role="presentation">
-                          ...{target} mais
-                        </span>
-                      ),
-                    }}
-                    onNavigate={(date) => {
-                      if (!hasNavigatedOnce) {
-                        this.setState({ hasNavigatedOnce: true });
-                        return;
-                      }
-                      this.setState({
-                        mes: date.getMonth() + 1,
-                        ano: date.getFullYear(),
-                      });
-                      this.getObjetosAsync({
-                        mes: date.getMonth() + 1,
-                        ano: date.getFullYear(),
-                      });
-                    }}
-                    defaultView={Views.MONTH}
-                  />
-                </Spin>
-                {currentEvent && (
-                  <>
-                    <ModalDadosObjeto
-                      showModal={showModalDadosObjeto}
-                      nomeObjetoNoCalendario={nomeObjeto}
-                      nomeObjetoNoCalendarioMinusculo={nomeObjetoMinusculo}
-                      closeModal={() =>
-                        this.setState({
-                          showModalDadosObjeto: false,
-                        })
-                      }
-                      objetos={objetos}
-                      event={currentEvent}
-                      getObjetosAsync={this.getObjetosAsync}
-                      setObjetoAsync={setObjeto}
-                    />
-                  </>
-                )}
-              </>
-            )}
-          </Spin>
-        </div>
+  return (
+    <div className="card calendario-sobremesa mt-3">
+      <div className="card-body">
+        <Spin tip="Carregando calendário..." spinning={!objetos && !erroAPI}>
+          {erroAPI && <div>{erroAPI}</div>}
+          {objetos && (
+            <>
+              <Spin
+                tip={`Carregando dias ${nomeObjeto}...`}
+                spinning={loadingDiasCalendario}
+              >
+                <DragAndDropCalendar
+                  tooltipAccessor={(e) => e.editais_numeros}
+                  style={{ height: 1000 }}
+                  formats={{
+                    weekdayFormat: (date, culture, localizer) =>
+                      localizer.format(date, "dddd", culture),
+                  }}
+                  selectable
+                  resizable={false}
+                  localizer={localizer}
+                  events={objetos}
+                  onSelectEvent={handleEvent}
+                  components={{
+                    toolbar: CustomToolbar,
+                  }}
+                  messages={{
+                    showMore: (target) => (
+                      <span className="ms-2" role="presentation">
+                        ...{target} mais
+                      </span>
+                    ),
+                  }}
+                  onNavigate={(date) => {
+                    if (!hasNavigatedOnce) {
+                      setHasNavigatedOnce(true);
+                      return;
+                    }
+                    setMes(date.getMonth() + 1);
+                    setAno(date.getFullYear());
+                    getObjetosAsync({
+                      mes: date.getMonth() + 1,
+                      ano: date.getFullYear(),
+                    });
+                  }}
+                  defaultView={Views.MONTH}
+                />
+              </Spin>
+              {currentEvent && (
+                <ModalDadosObjeto
+                  showModal={showModalDadosObjeto}
+                  nomeObjetoNoCalendario={nomeObjeto}
+                  nomeObjetoNoCalendarioMinusculo={nomeObjetoMinusculo}
+                  closeModal={() => setShowModalDadosObjeto(false)}
+                  objetos={objetos}
+                  event={currentEvent}
+                  getObjetosAsync={getObjetosAsync}
+                  setObjetoAsync={setObjeto}
+                />
+              )}
+            </>
+          )}
+        </Spin>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
