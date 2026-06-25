bd4f3ab5fedd4b145731f407f540c424765ac2d9
Claudinei <claudinei.silva@spassu.com.br>
2026-06-25 11:33:52 -0300

    test(149152): cobre comportamento ponto a ponto

--- src/components/Shareable/__tests__/CardCronograma/CardCronograma.test.jsx
+++ src/components/Shareable/__tests__/CardCronograma/CardCronograma.test.jsx
@@ -27,6 +27,13 @@ describe("CardCronograma", () => {
     tipo_entrega: "ARMAZEM",
   };
 
+  const solicitacaoPontoAPontoLeveLeite = {
+    ...solicitacaoPontoAPonto,
+    text: "Solicitação Ponto a Ponto Leve Leite",
+    fullText: "Texto completo Ponto a Ponto Leve Leite",
+    programa_leve_leite: true,
+  };
+
   test("Deve aplicar a classe 'point-to-point' quando o tipo de entrega for Ponto a Ponto", () => {
     render(
       <MemoryRouter>
@@ -47,6 +54,7 @@ describe("CardCronograma", () => {
       .closest("p");
 
     expect(paragraph).toHaveClass("point-to-point");
+    expect(paragraph).not.toHaveClass("programa-leve-leite");
   });
 
   test("Não deve aplicar a classe 'point-to-point' quando o tipo de entrega for Armazém", () => {
@@ -67,5 +75,29 @@ describe("CardCronograma", () => {
     const paragraph = screen.getByText("Solicitação Armazém").closest("p");
 
     expect(paragraph).not.toHaveClass("point-to-point");
+    expect(paragraph).not.toHaveClass("programa-leve-leite");
+  });
+
+  test("Deve aplicar as classes de Ponto a Ponto e Leve Leite quando ambas as condições forem atendidas", () => {
+    render(
+      <MemoryRouter>
+        <CardCronograma
+          cardTitle="Cronograma"
+          cardType="tipo-mock"
+          solicitations={[solicitacaoPontoAPontoLeveLeite]}
+          icon="fa-calendar"
+          loading={false}
+          href="/mock-href"
+          exibirTooltip={false}
+        />
+      </MemoryRouter>,
+    );
+
+    const paragraph = screen
+      .getByText("Solicitação Ponto a Ponto Leve Leite")
+      .closest("p");
+
+    expect(paragraph).toHaveClass("programa-leve-leite");
+    expect(paragraph).toHaveClass("point-to-point");
   });
 });
