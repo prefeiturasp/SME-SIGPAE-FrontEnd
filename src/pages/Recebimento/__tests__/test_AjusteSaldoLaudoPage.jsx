eeeb3184d8842f3c3cd6016e5990c811265381ca
Murilo Ferri Schirmer <murilo.schirmer@spassu.com.br>
2026-06-29 01:43:14 -0300

    test(150765): :white_check_mark: implementa teste unitário da listagem e dos filtros

--- src/pages/Recebimento/__tests__/test_AjusteSaldoLaudoPage.jsx
+++ src/pages/Recebimento/__tests__/test_AjusteSaldoLaudoPage.jsx
@@ -8,27 +8,35 @@ import {
 import "@testing-library/jest-dom";
 import mock from "src/services/_mock";
 import { MemoryRouter } from "react-router-dom";
-import CadastroAjusteSaldoPage from "src/pages/Recebimento/AjusteSaldoLaudo/CadastroAjusteSaldoPage";
+import AjusteSaldoLaudoPage from "src/pages/Recebimento/AjusteSaldoLaudo/AjusteSaldoLaudoPage";
 import { MeusDadosContext } from "src/context/MeusDadosContext";
 import { mockMeusDadosDilogQualidade } from "src/mocks/meusDados/dilog-qualidade";
 
-import { mockGetCronogramasComDocs } from "src/mocks/services/ajusteSaldo.service/mockGetCronogramasComDocs";
-import { mockGetDocumentosCronograma } from "src/mocks/services/ajusteSaldo.service/mockGetDocumentosCronograma";
+import { mockListaSimplesTerceirizadas } from "src/mocks/services/terceirizada.service/mockListaSimplesTerceirizadas";
+import { mockGetListaCompletaProdutosLogistica } from "src/mocks/produto.service/mockGetListaCompletaProdutosLogistica";
+import {
+  mockGetListagemAjustes,
+  mockGetListagemAjustesFiltrados,
+} from "../../../mocks/services/ajusteSaldo.service/mockGetListagemAjustes";
 
 import { PERFIL, TIPO_PERFIL } from "src/constants/shared";
 
-describe("Testar Listagem de Fichas de Recebimento", () => {
+import { debug } from "jest-preview";
+
+describe("Testar Listagem dos Ajustes de Saldo", () => {
   beforeEach(async () => {
     localStorage.setItem("perfil", PERFIL.DILOG_QUALIDADE);
     localStorage.setItem("tipo_perfil", TIPO_PERFIL.PRE_RECEBIMENTO);
 
     mock
-      .onGet(`/ajuste-saldo-laudo/cronogramas-mensal-com-documentos/`)
-      .reply(200, mockGetCronogramasComDocs);
+      .onGet(`/cadastro-produtos-edital/lista-completa-logistica/`)
+      .reply(200, mockGetListaCompletaProdutosLogistica);
 
     mock
-      .onGet(`ajuste-saldo-laudo/documentos-do-cronograma/`)
-      .reply(200, mockGetDocumentosCronograma);
+      .onGet(`/terceirizadas/lista-simples/`)
+      .reply(200, mockListaSimplesTerceirizadas);
+
+    mock.onGet(`/ajuste-saldo-laudo/`).reply(200, mockGetListagemAjustes);
 
     await act(async () => {
       render(
@@ -44,56 +52,49 @@ describe("Testar Listagem de Fichas de Recebimento", () => {
               setMeusDados: jest.fn(),
             }}
           >
-            <CadastroAjusteSaldoPage />
+            <AjusteSaldoLaudoPage />
           </MeusDadosContext.Provider>
         </MemoryRouter>,
       );
     });
   });
 
-  it("Testa o preenchimento do formulário", async () => {
-    const cronogramaNumero = "156/2024A";
-    const cronograma = mockGetCronogramasComDocs.find(
-      (c) => c.numero === cronogramaNumero,
+  it("Testa a renderização dos elementos da página", async () => {
+    expect(
+      screen.getByText("Ajustes de Saldo Cadastrados"),
+    ).toBeInTheDocument();
+
+    expect(screen.getByText("Nº do Cronograma")).toBeInTheDocument();
+    expect(screen.getByText("Produto")).toBeInTheDocument();
+    expect(screen.getByText("Fornecedor")).toBeInTheDocument();
+    expect(screen.getByText("Nº do Laudo")).toBeInTheDocument();
+    expect(screen.getByText("Quantidade a ser Descontada")).toBeInTheDocument();
+
+    const botaoCadastro = screen
+      .getByText("Cadastrar Saldo do Laudo")
+      .closest("button");
+    expect(botaoCadastro).toBeInTheDocument();
+
+    debug();
+  });
+
+  it("Testa o funcionamento dos filtros", async () => {
+    mock
+      .onGet(`/ajuste-saldo-laudo/`)
+      .reply(200, mockGetListagemAjustesFiltrados);
+
+    const inputFiltro = screen.getByPlaceholderText(
+      "Digite o Nº do Cronograma",
     );
-    const documento = mockGetDocumentosCronograma[0];
+    fireEvent.change(inputFiltro, { target: { value: "039" } });
 
-    const inputCronograma = screen.getByTestId("cronograma");
-    await act(async () => {
-      fireEvent.mouseDown(inputCronograma);
+    const botaoFiltrar = screen.getByText("Filtrar").closest("button");
+    expect(botaoFiltrar).toBeInTheDocument();
+    fireEvent.click(botaoFiltrar);
+
+    await waitFor(() => {
+      expect(screen.getByText("039/2023")).toBeInTheDocument();
+      expect(screen.queryByText("156/2024A")).not.toBeInTheDocument();
     });
-
-    await waitFor(() => screen.getByText("156/2024A"));
-    await act(async () => {
-      fireEvent.click(screen.getByText("156/2024A"));
-    });
-
-    const produtoInput = await screen.findByDisplayValue(
-      cronograma.produto_nome,
-    );
-    expect(produtoInput).toBeInTheDocument();
-
-    const fornecedorInput = await screen.findByDisplayValue(
-      cronograma.fornecedor_nome,
-    );
-    expect(fornecedorInput).toBeInTheDocument();
-
-    const inputLaudo = screen.getByTestId("numero-laudo");
-    await act(async () => {
-      fireEvent.mouseDown(inputLaudo);
-    });
-
-    await waitFor(() => screen.getAllByText("12345").length > 0);
-    await act(async () => {
-      fireEvent.click(screen.getAllByText("12345")[1]);
-    });
-
-    const inputQuantidade = screen.getByPlaceholderText("Digite a Quantidade");
-    fireEvent.change(inputQuantidade, { target: { value: "10,00" } });
-    fireEvent.blur(inputQuantidade);
-
-    const expectedSaldo = (parseFloat(documento.saldo_atual) - 10).toFixed(2);
-    const saldoAtualInput = await screen.findByDisplayValue(expectedSaldo);
-    expect(saldoAtualInput).toBeInTheDocument();
   });
 });
