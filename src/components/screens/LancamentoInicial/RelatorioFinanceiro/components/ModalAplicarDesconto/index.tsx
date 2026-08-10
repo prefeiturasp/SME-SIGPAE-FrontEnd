52fa3ff833fdb21116308749185d2c0a9e7bb241
GuiLMassini <guimassinifai@gmail.com>
2026-08-10 17:22:17 -0300

    refact(140391): separacao de helpers e melhoria de codigo

--- src/components/screens/LancamentoInicial/RelatorioFinanceiro/components/ModalAplicarDesconto/index.tsx
+++ src/components/screens/LancamentoInicial/RelatorioFinanceiro/components/ModalAplicarDesconto/index.tsx
@@ -35,8 +35,12 @@ import {
 } from "src/helpers/parsers";
 import { capitalize } from "src/helpers/utilities";
 import ModalCancelar from "../ModalCancelar";
-import { getUuid } from "../../helpers";
 import { ClausulaInterface } from "src/interfaces/clausulas_para_descontos.interface";
+import {
+  formatarPayload,
+  getValoresDescontos,
+  getValorUnitario,
+} from "./helpers";
 
 const DEFAULT_EMPENHO: DescontoFinanceiro = {
   unidades_educacionais: [],
@@ -93,7 +97,7 @@ const ModalAplicarDesconto = ({
   const [cancelar, setCancelar] = useState<boolean>(false);
 
   const grupoNome =
-    relatorioConsolidado?.grupo_unidade_escolar?.nome.toUpperCase();
+    relatorioConsolidado?.grupo_unidade_escolar?.nome?.toUpperCase() ?? "";
 
   const ehCei = grupoNome?.includes("GRUPO 1");
   const ehCemei = grupoNome?.includes("GRUPO 2");
@@ -110,58 +114,14 @@ const ModalAplicarDesconto = ({
     ["LANCHE", "LANCHE 4H"].includes(normalizar(item.nome).toUpperCase()),
   );
 
-  const getValoresDescontos = (
-    desconto: DescontoFinanceiro,
-  ): DescontoFinanceiro => {
-    let tipoAlimentacao = getUuid(desconto.tipo_alimentacao);
-
-    if (!desconto.tipo_alimentacao && !desconto.faixa_etaria) {
-      tipoAlimentacao = "kit_lanche";
-    } else if (ehEmef && desconto.periodo_escolar) {
-      tipoAlimentacao = `${desconto.periodo_escolar}|${tipoAlimentacao}`;
-    }
-
-    const payload: DescontoFinanceiro = {
-      ...desconto,
-      faixa_etaria:
-        desconto.faixa_etaria && desconto.periodo_escolar
-          ? `${desconto.periodo_escolar}|${getUuid(desconto.faixa_etaria)}`
-          : null,
-      tipo_alimentacao: tipoAlimentacao,
-      clausula_desconto: getUuid(desconto.clausula_desconto),
-      unidades_educacionais: desconto.unidades_educacionais?.map(getUuid) ?? [],
-    };
-
-    if (desconto.cei_ou_emei && desconto.cei_ou_emei !== "N/A") {
-      return {
-        ...payload,
-        tipo_lancamento: `${desconto.cei_ou_emei}|${getUuid(
-          desconto.tipo_lancamento,
-        )}`,
-      };
-    } else if (
-      desconto.infantil_ou_fundamental &&
-      desconto.infantil_ou_fundamental !== "N/A"
-    ) {
-      return {
-        ...payload,
-        tipo_lancamento: `${desconto.infantil_ou_fundamental}|${getUuid(
-          desconto.tipo_lancamento,
-        )}`,
-      };
-    }
-
-    return payload;
-  };
-
   const initialValues = useMemo(() => {
     return {
       cadastros_desconto:
         descontos?.length > 0
-          ? descontos.map(getValoresDescontos)
+          ? descontos.map((e) => getValoresDescontos(e, { ehEmef }))
           : [DEFAULT_EMPENHO],
     };
-  }, [descontos]);
+  }, [descontos, ehEmef]);
 
   const faixasEtariasOptions = useMemo(() => {
     return [
@@ -249,68 +209,14 @@ const ModalAplicarDesconto = ({
     return [{ uuid: "", nome: "Selecione o tipo" }, ...TIPO_LANCAMENTO_OPTIONS];
   };
 
-  const formatarPayload = (
-    descontos: DescontoFinanceiro[],
-  ): DescontoFinanceiro[] =>
-    descontos.map((desconto) => {
-      let payload = {
-        ...desconto,
-        periodo_escolar: null,
-      };
-
-      if (typeof desconto.faixa_etaria === "string") {
-        const [periodo, faixaEtaria] = desconto.faixa_etaria.split("|");
-
-        payload = {
-          ...payload,
-          periodo_escolar: periodo,
-          faixa_etaria: faixaEtaria,
-        };
-      }
-
-      if (
-        ehEmef &&
-        typeof desconto.tipo_alimentacao === "string" &&
-        desconto.tipo_alimentacao.includes("NOITE")
-      ) {
-        const [periodo, tipoAlimentacao] = desconto.tipo_alimentacao.split("|");
-
-        payload = {
-          ...payload,
-          periodo_escolar: periodo,
-          tipo_alimentacao: tipoAlimentacao,
-        };
-      }
-
-      if (ehCemei) {
-        const [tipoUnidade, tipoLancamento] =
-          desconto.tipo_lancamento?.split("|") ?? [];
-
-        payload = {
-          ...payload,
-          tipo_lancamento: tipoLancamento,
-          cei_ou_emei: tipoUnidade,
-        };
-      }
-
-      if (ehEmebs) {
-        const [turma, tipoLancamento] =
-          desconto.tipo_lancamento?.split("|") ?? [];
-
-        payload = {
-          ...payload,
-          tipo_lancamento: tipoLancamento,
-          infantil_ou_fundamental: turma,
-        };
-      }
-
-      return payload;
-    });
-
   const onSubmit = async (values: {
     cadastros_desconto: DescontoFinanceiro[];
   }) => {
-    const payload = formatarPayload(values?.cadastros_desconto ?? []);
+    const payload = formatarPayload(values?.cadastros_desconto ?? [], {
+      ehCemei,
+      ehEmebs,
+      ehEmef,
+    });
 
     const response = await cadastroDescontoFinanceiro(
       payload,
@@ -342,104 +248,6 @@ const ModalAplicarDesconto = ({
     if (relatorioConsolidado) getClausulasParaDescontosAsync();
   }, [relatorioConsolidado]);
 
-  const getValorUnitario = (
-    tipoLancamento: string,
-    faixaSelecionada: string,
-    alimentacaoSelecionada: string,
-  ) => {
-    if (!tipoLancamento) return 0;
-
-    const [tipo, lancamento] = tipoLancamento.includes("|")
-      ? tipoLancamento.split("|")
-      : [null, tipoLancamento];
-
-    let campo = null;
-
-    if ((ehCei || ehCemei) && faixaSelecionada) {
-      const [periodo, faixaUuid] = faixaSelecionada.split("|");
-
-      const faixa = faixasEtarias.find((f) => f.uuid === faixaUuid);
-
-      if (!faixa) return 0;
-
-      const nomeFaixa = normalizar(faixa.__str__);
-
-      const tabela = relatorioConsolidado.tabelas?.find((tabela) => {
-        const nomeTabela = tabela.nome
-          ?.normalize("NFD")
-          .replace(/[\u0300-\u036f]/g, "")
-          .toUpperCase();
-
-        return (
-          nomeTabela?.includes(lancamento.replaceAll("_", " ").toUpperCase()) &&
-          tabela.periodo_escolar === periodo
-        );
-      });
-
-      if (!tabela) return 0;
-
-      campo = tabela.valores?.find(
-        (item) =>
-          normalizar(item.nome_campo).replaceAll("_", " ") === nomeFaixa &&
-          item.tipo_valor === "UNITARIO",
-      );
-    } else if (alimentacaoSelecionada) {
-      let nomeTipo = "";
-      const ehRefeicaoEja = alimentacaoSelecionada.includes("NOITE");
-
-      if (alimentacaoSelecionada === "kit_lanche") {
-        nomeTipo = "kit lanche";
-      } else {
-        const tipoAlimentacao = tiposAlimentacao.find(
-          (t) =>
-            t.uuid ===
-            (ehRefeicaoEja
-              ? alimentacaoSelecionada.replace("NOITE|", "")
-              : alimentacaoSelecionada),
-        );
-
-        nomeTipo = normalizar(tipoAlimentacao?.nome);
-      }
-
-      const tabela = relatorioConsolidado.tabelas?.find((tabela) => {
-        const nomeTabela = tabela.nome
-          ?.normalize("NFD")
-          .replace(/[\u0300-\u036f]/g, "")
-          .toUpperCase();
-
-        const corresponde =
-          nomeTabela?.includes(lancamento.replaceAll("_", " ").toUpperCase()) &&
-          (!ehEmebs || nomeTabela?.includes(tipo?.toUpperCase()));
-
-        return (
-          corresponde &&
-          tabela.periodo_escolar === (ehRefeicaoEja ? "NOITE" : null)
-        );
-      });
-
-      if (!tabela) return 0;
-
-      if (!ehEmef || !nomeTipo.includes("refeicao")) {
-        campo = tabela.valores?.find(
-          (item) =>
-            normalizar(item.nome_campo).replaceAll("_", " ") === nomeTipo &&
-            item.tipo_valor === "UNITARIO",
-        );
-      } else {
-        const tipoRefeicao = ehRefeicaoEja ? "eja" : "emef";
-
-        campo = tabela.valores?.find(
-          (item) =>
-            normalizar(item.nome_campo)
-              .replaceAll("_", " ")
-              .includes(tipoRefeicao) && item.tipo_valor === "UNITARIO",
-        );
-      }
-    }
-
-    return stringDecimalToNumber(campo?.valor) ?? 0;
-  };
-
   return (
     <Modal show={showModal} onHide={() => setCancelar(true)} size="lg">
       <Modal.Header closeButton>
@@ -453,11 +261,18 @@ const ModalAplicarDesconto = ({
         render={({ handleSubmit, submitting, values, form }) => {
           useEffect(() => {
             values.cadastros_desconto?.forEach((desconto, index) => {
-              const valorUnitario = getValorUnitario(
-                desconto.tipo_lancamento,
-                desconto.faixa_etaria,
-                desconto.tipo_alimentacao,
-              );
+              const valorUnitario = getValorUnitario({
+                desconto,
+                grupo: {
+                  ehCei,
+                  ehCemei,
+                  ehEmebs,
+                  ehEmef,
+                },
+                faixasEtarias,
+                tiposAlimentacao,
+                tabelas: relatorioConsolidado.tabelas,
+              });
 
               const clausula = clausulas.find(
                 (c) => c.uuid === desconto.clausula_desconto,
