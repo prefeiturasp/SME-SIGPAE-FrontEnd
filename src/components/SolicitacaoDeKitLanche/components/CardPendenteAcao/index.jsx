96f077c81b83883ddca9d74ba39e134d19d88645
Calvin Rossinhole <calvin.masters@gmail.com>
2026-09-21 11:11:33 -0300

    fix(-): :bug: corrige exibicao de busca nos cards

--- src/components/SolicitacaoDeKitLanche/components/CardPendenteAcao/index.jsx
+++ src/components/SolicitacaoDeKitLanche/components/CardPendenteAcao/index.jsx
@@ -113,147 +113,152 @@ export class CardPendenteAcao extends Component {
             )}
           </div>
         </div>
-        <Collapse isOpened={!collapsed}>
-          <div className="row">
-            <div className="col-12">
-              <div className="input-search-full-width">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Pesquisar"
-                  value={this.props.busca}
-                  onChange={this.filtrarPedidos}
-                />
-                <i className="fas fa-search inside-input" />
+        {pedidos.length > 0 && (
+          <Collapse isOpened={!collapsed}>
+            <div className="row">
+              <div className="col-12">
+                <div className="input-search-full-width">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Pesquisar"
+                    value={this.props.busca}
+                    onChange={this.filtrarPedidos}
+                  />
+                  <i className="fas fa-search inside-input" />
+                </div>
+                <small
+                  className="form-text text-muted"
+                  style={{ fontSize: 10 }}
+                >
+                  Pesquisa por: código do pedido, código EOL e nome da escola
+                </small>
               </div>
-              <small className="form-text text-muted" style={{ fontSize: 10 }}>
-                Pesquisa por: código do pedido, código EOL e nome da escola
-              </small>
-            </div>
-            <table className="orders-table mt-4 ms-3 me-3">
-              <thead>
-                <tr className="row">
-                  <th className="col-2">Código do Pedido</th>
-                  <th className="col-2">Código EOL</th>
-                  <th className="col-3">Nome da Escola</th>
-                  <th className="col-3">{ultimaColunaLabel || "Data"}</th>
-                  <th className="col-2">Solic. Similares</th>
-                </tr>
-              </thead>
-              <tbody>
-                {pedidosFiltrados.length > 0 &&
-                  pedidosFiltrados.map((pedido, key) => {
-                    const tipoSolicitacao = ehEscolaTipoCEI(pedido.escola)
-                      ? SOLICITACAO_CEI
-                      : ehEscolaTipoCEMEI(pedido.escola)
-                        ? SOLICITACAO_CEMEI
-                        : SOLICITACAO_NORMAL;
-                    const solicitacaoUrl = ehEscolaTipoCEMEI(pedido.escola)
-                      ? SOLICITACAO_KIT_LANCHE_CEMEI
-                      : SOLICITACAO_KIT_LANCHE;
-                    return (
-                      <>
-                        <tr className="row">
-                          <td className="col-2">
-                            <Link
-                              className="text-dark"
-                              key={key}
-                              to={`/${solicitacaoUrl}/relatorio?uuid=${pedido.uuid}&tipoSolicitacao=${tipoSolicitacao}`}
-                            >
-                              {pedido.id_externo}
-                            </Link>
-                          </td>
-                          <td className="col-2">
-                            <Link
-                              className="text-dark"
-                              key={key}
-                              to={`/${solicitacaoUrl}/relatorio?uuid=${pedido.uuid}&tipoSolicitacao=${tipoSolicitacao}`}
-                            >
-                              {pedido.escola.codigo_eol}
-                            </Link>
-                          </td>
-                          <td className="col-3">
-                            <Link
-                              className="text-dark"
-                              key={key}
-                              to={`/${solicitacaoUrl}/relatorio?uuid=${pedido.uuid}&tipoSolicitacao=${tipoSolicitacao}`}
-                            >
-                              {pedido.escola.nome}
-                            </Link>
-                          </td>
-                          <td className="col-3">
-                            <Link
-                              className="text-dark"
-                              key={key}
-                              to={`/${solicitacaoUrl}/relatorio?uuid=${pedido.uuid}&tipoSolicitacao=${tipoSolicitacao}`}
-                            >
-                              {pedido.solicitacao_kit_lanche
-                                ? pedido.solicitacao_kit_lanche.data
-                                : pedido.data}
-                            </Link>
-                          </td>
-                          <td className="col-2 solicitacao-consolidada-collapse">
-                            {pedido.solicitacoes_similares.length ? (
-                              pedido.solicitacoes_similares.map(
-                                (s, idxSolicitacaoSimilar) => {
-                                  return (
-                                    <p
-                                      className="gatilho-style"
-                                      key={idxSolicitacaoSimilar}
-                                    >
-                                      <i
-                                        className="fa fa-info-circle me-1"
-                                        aria-hidden="true"
-                                      />
-                                      <b>
-                                        {`#${s.id_externo}`}
-                                        <ToggleExpandir
-                                          onClick={() =>
-                                            this.collapseSolicitacaoSimilar(
-                                              key,
-                                              idxSolicitacaoSimilar,
-                                            )
-                                          }
-                                          ativo={!s.collapsed}
-                                          className="icon-padding"
+              <table className="orders-table mt-4 ms-3 me-3">
+                <thead>
+                  <tr className="row">
+                    <th className="col-2">Código do Pedido</th>
+                    <th className="col-2">Código EOL</th>
+                    <th className="col-3">Nome da Escola</th>
+                    <th className="col-3">{ultimaColunaLabel || "Data"}</th>
+                    <th className="col-2">Solic. Similares</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {pedidosFiltrados.length > 0 &&
+                    pedidosFiltrados.map((pedido, key) => {
+                      const tipoSolicitacao = ehEscolaTipoCEI(pedido.escola)
+                        ? SOLICITACAO_CEI
+                        : ehEscolaTipoCEMEI(pedido.escola)
+                          ? SOLICITACAO_CEMEI
+                          : SOLICITACAO_NORMAL;
+                      const solicitacaoUrl = ehEscolaTipoCEMEI(pedido.escola)
+                        ? SOLICITACAO_KIT_LANCHE_CEMEI
+                        : SOLICITACAO_KIT_LANCHE;
+                      return (
+                        <>
+                          <tr className="row">
+                            <td className="col-2">
+                              <Link
+                                className="text-dark"
+                                key={key}
+                                to={`/${solicitacaoUrl}/relatorio?uuid=${pedido.uuid}&tipoSolicitacao=${tipoSolicitacao}`}
+                              >
+                                {pedido.id_externo}
+                              </Link>
+                            </td>
+                            <td className="col-2">
+                              <Link
+                                className="text-dark"
+                                key={key}
+                                to={`/${solicitacaoUrl}/relatorio?uuid=${pedido.uuid}&tipoSolicitacao=${tipoSolicitacao}`}
+                              >
+                                {pedido.escola.codigo_eol}
+                              </Link>
+                            </td>
+                            <td className="col-3">
+                              <Link
+                                className="text-dark"
+                                key={key}
+                                to={`/${solicitacaoUrl}/relatorio?uuid=${pedido.uuid}&tipoSolicitacao=${tipoSolicitacao}`}
+                              >
+                                {pedido.escola.nome}
+                              </Link>
+                            </td>
+                            <td className="col-3">
+                              <Link
+                                className="text-dark"
+                                key={key}
+                                to={`/${solicitacaoUrl}/relatorio?uuid=${pedido.uuid}&tipoSolicitacao=${tipoSolicitacao}`}
+                              >
+                                {pedido.solicitacao_kit_lanche
+                                  ? pedido.solicitacao_kit_lanche.data
+                                  : pedido.data}
+                              </Link>
+                            </td>
+                            <td className="col-2 solicitacao-consolidada-collapse">
+                              {pedido.solicitacoes_similares.length ? (
+                                pedido.solicitacoes_similares.map(
+                                  (s, idxSolicitacaoSimilar) => {
+                                    return (
+                                      <p
+                                        className="gatilho-style"
+                                        key={idxSolicitacaoSimilar}
+                                      >
+                                        <i
+                                          className="fa fa-info-circle me-1"
+                                          aria-hidden="true"
                                         />
-                                      </b>
-                                    </p>
-                                  );
-                                },
-                              )
-                            ) : (
-                              <p />
+                                        <b>
+                                          {`#${s.id_externo}`}
+                                          <ToggleExpandir
+                                            onClick={() =>
+                                              this.collapseSolicitacaoSimilar(
+                                                key,
+                                                idxSolicitacaoSimilar,
+                                              )
+                                            }
+                                            ativo={!s.collapsed}
+                                            className="icon-padding"
+                                          />
+                                        </b>
+                                      </p>
+                                    );
+                                  },
+                                )
+                              ) : (
+                                <p />
+                              )}
+                            </td>
+                          </tr>
+                          {pedido.solicitacoes_similares.length > 0 &&
+                            pedido.solicitacoes_similares.map(
+                              (s, idxSolicitacaoSimilar) => {
+                                return (
+                                  <SolicitacoesSimilaresKitLanche
+                                    key={idxSolicitacaoSimilar}
+                                    solicitacao={s}
+                                    index={idxSolicitacaoSimilar}
+                                  />
+                                );
+                              },
                             )}
-                          </td>
-                        </tr>
-                        {pedido.solicitacoes_similares.length > 0 &&
-                          pedido.solicitacoes_similares.map(
-                            (s, idxSolicitacaoSimilar) => {
-                              return (
-                                <SolicitacoesSimilaresKitLanche
-                                  key={idxSolicitacaoSimilar}
-                                  solicitacao={s}
-                                  index={idxSolicitacaoSimilar}
-                                />
-                              );
-                            },
-                          )}
-                      </>
-                    );
-                  })}
-              </tbody>
-            </table>
-            {totalSolicitacoes > PAGE_SIZE && (
-              <Paginacao
-                current={this.props.page}
-                pageSize={PAGE_SIZE}
-                total={totalSolicitacoes}
-                onChange={this.props.onPageChange}
-              />
-            )}
-          </div>
-        </Collapse>
+                        </>
+                      );
+                    })}
+                </tbody>
+              </table>
+              {totalSolicitacoes > PAGE_SIZE && (
+                <Paginacao
+                  current={this.props.page}
+                  pageSize={PAGE_SIZE}
+                  total={totalSolicitacoes}
+                  onChange={this.props.onPageChange}
+                />
+              )}
+            </div>
+          </Collapse>
+        )}
       </div>
     );
   }
