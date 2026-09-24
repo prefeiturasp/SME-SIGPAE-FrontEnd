9b76efe8db5b6ab547d4a8d0416dcd0d8267e14f
Calvin Rossinhole <calvin.masters@gmail.com>
2026-09-24 15:19:56 -0300

    fix(-): :bug: corrige breadcrumb painel produtos

--- src/pages/DashboardGestaoProduto/DashboardGestaoProdutoPage.jsx
+++ src/pages/DashboardGestaoProduto/DashboardGestaoProdutoPage.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Breadcrumb from "src/components/Shareable/Breadcrumb";
 import { DashboardGestaoProduto } from "src/components/screens/DashboardGestaoProduto";
+import { PAINEL_GESTAO_PRODUTO } from "src/configs/constants";
 import Page from "src/components/Shareable/Page/Page";
 
 const DashboardGestaoProdutoPage = (props) => {
   return (
     <Page titulo="Acompanhamento de produtos cadastrados">
-      <Breadcrumb home={"/"} />
+      <Breadcrumb
+        home={"/"}
+        atual={{
+          href: `/${PAINEL_GESTAO_PRODUTO}`,
+          titulo: "Painel de Solicitações",
+        }}
+      />
       <DashboardGestaoProduto {...props} />
     </Page>
   );
