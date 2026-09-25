5dcb42ba5e3dc7da44368e1fd11284bdb903cd2f
Calvin Rossinhole <calvin.masters@gmail.com>
2026-09-25 10:38:50 -0300

    refactor(155180): :hammer: converte CardPendenteAcao para function component

--- src/components/AlteracaoDeCardapio/components/CardPendenteAcao/index.jsx
+++ src/components/AlteracaoDeCardapio/components/CardPendenteAcao/index.jsx
@@ -1,27 +1,27 @@
-import { gerarLinkRelatorio, talvezPluralizar } from "src/helpers/utilities";
-import React, { Component } from "react";
+import { useState } from "react";
 import { Collapse } from "react-collapse";
 import { Link } from "react-router-dom";
+import "src/components/Shareable/style.scss";
+import { ToggleExpandir } from "src/components/Shareable/ToggleExpandir";
 import {
   ALTERACAO_TIPO_ALIMENTACAO,
   ALTERACAO_TIPO_ALIMENTACAO_CEMEI,
-} from "../../../../configs/constants";
-import "../../../Shareable/style.scss";
-import { ToggleExpandir } from "../../../Shareable/ToggleExpandir";
+} from "src/configs/constants";
+import { gerarLinkRelatorio, talvezPluralizar } from "src/helpers/utilities";
 import { calcularNumeroDeEscolasUnicas } from "./helper";
 
-export class CardPendenteAcao extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collapsed: true,
-      pedidosFiltrados: this.props.pedidos,
-    };
-    this.filtrarPedidos = this.filtrarPedidos.bind(this);
-  }
+export const CardPendenteAcao = ({
+  pedidos,
+  titulo,
+  tipoDeCard,
+  ultimaColunaLabel,
+  dataTestId,
+}) => {
+  const [collapsed, setCollapsed] = useState(true);
+  const [pedidosFiltrados, setPedidosFiltrados] = useState(pedidos);
 
-  filtrarPedidos(event) {
-    let pedidosFiltrados = this.props.pedidos;
+  const filtrarPedidos = (event) => {
+    let pedidosFiltrados = pedidos;
     pedidosFiltrados = pedidosFiltrados.filter(function (item) {
       const palavraAFiltrar = event.target.value.toLowerCase();
       return (
@@ -30,34 +30,30 @@ export class CardPendenteAcao extends Component {
         item.escola.codigo_eol.includes(palavraAFiltrar)
       );
     });
-    this.setState({ pedidosFiltrados });
-  }
+    setPedidosFiltrados(pedidosFiltrados);
+  };
 
-  render() {
-    const { pedidos, titulo, tipoDeCard, ultimaColunaLabel, dataTestId } =
-      this.props;
-    const { collapsed, pedidosFiltrados } = this.state;
-    return (
-      <div className="card card-pendency-approval" data-testid={dataTestId}>
-        <div className={"card-title " + tipoDeCard}>{titulo}</div>
-        <div className="row">
-          <div className="col-2">
-            <div className={"order-box " + tipoDeCard}>
-              <span className="number">{pedidos.length}</span>
-              <span className="order">
-                {pedidos.length === 1 ? "solicitação" : "solicitações"}
-              </span>
-            </div>
+  return (
+    <div className="card card-pendency-approval" data-testid={dataTestId}>
+      <div className={"card-title " + tipoDeCard}>{titulo}</div>
+      <div className="row">
+        <div className="col-2">
+          <div className={"order-box " + tipoDeCard}>
+            <span className="number">{pedidos.length}</span>
+            <span className="order">
+              {pedidos.length === 1 ? "solicitação" : "solicitações"}
+            </span>
           </div>
-          {pedidos.length > 0 && (
-            <div className="col-9">
-              <div className="order-lines">
-                <div className="label" />
-                <span className="text">
-                  <span className="value">
-                    {calcularNumeroDeEscolasUnicas(pedidos)}{" "}
-                  </span>
-                  {`
+        </div>
+        {pedidos.length > 0 && (
+          <div className="col-9">
+            <div className="order-lines">
+              <div className="label" />
+              <span className="text">
+                <span className="value">
+                  {calcularNumeroDeEscolasUnicas(pedidos)}{" "}
+                </span>
+                {`
                   ${talvezPluralizar(
                     calcularNumeroDeEscolasUnicas(pedidos),
                     "escola",
@@ -66,72 +62,71 @@ export class CardPendenteAcao extends Component {
                     "solicitante",
                   )}
                   `}
-                </span>
-              </div>
+              </span>
             </div>
+          </div>
+        )}
+        <div className="col-1">
+          {pedidos.length > 0 && (
+            <ToggleExpandir
+              onClick={() => setCollapsed(!collapsed)}
+              ativo={!collapsed}
+            />
           )}
-          <div className="col-1">
-            {pedidos.length > 0 && (
-              <ToggleExpandir
-                onClick={() => this.setState({ collapsed: !collapsed })}
-                ativo={!collapsed}
-              />
-            )}
-          </div>
         </div>
-        <Collapse isOpened={!collapsed}>
-          <div className="row">
-            <div className="input-search-full-width col-12">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Pesquisar"
-                onChange={this.filtrarPedidos}
-              />
-              <i className="fas fa-search inside-input" />
-            </div>
-            <table className="orders-table mt-4 ms-3 me-3">
-              <thead>
-                <tr className="row">
-                  <th className="col-3">Código do Pedido</th>
-                  <th className="col-3">Código EOL</th>
-                  <th className="col-3">Nome da Escola</th>
-                  <th className="col-3">{ultimaColunaLabel || "Data"}</th>
-                </tr>
-              </thead>
-              <tbody>
-                {pedidosFiltrados.map((pedido, key) => {
-                  const dataMaisProxima =
-                    pedido.inclusoes && pedido.inclusoes[0].data;
-                  return (
-                    <Link
-                      key={key}
-                      to={gerarLinkRelatorio(
-                        pedido.alunos_cei_e_ou_emei
-                          ? ALTERACAO_TIPO_ALIMENTACAO_CEMEI
-                          : ALTERACAO_TIPO_ALIMENTACAO,
-                        pedido,
-                      )}
-                    >
-                      <tr className="row">
-                        <td className="col-3">{pedido.id_externo}</td>
-                        <td className="col-3">{pedido.escola.codigo_eol}</td>
-                        <td className="col-3">{pedido.escola.nome}</td>
-                        <td className="col-3">
-                          {pedido.data_inicial ||
-                            pedido.alterar_dia ||
-                            dataMaisProxima ||
-                            pedido.data}
-                        </td>
-                      </tr>
-                    </Link>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-        </Collapse>
       </div>
-    );
-  }
-}
+      <Collapse isOpened={!collapsed}>
+        <div className="row">
+          <div className="input-search-full-width col-12">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Pesquisar"
+              onChange={filtrarPedidos}
+            />
+            <i className="fas fa-search inside-input" />
+          </div>
+          <table className="orders-table mt-4 ms-3 me-3">
+            <thead>
+              <tr className="row">
+                <th className="col-3">Código do Pedido</th>
+                <th className="col-3">Código EOL</th>
+                <th className="col-3">Nome da Escola</th>
+                <th className="col-3">{ultimaColunaLabel || "Data"}</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pedidosFiltrados.map((pedido, key) => {
+                const dataMaisProxima =
+                  pedido.inclusoes && pedido.inclusoes[0].data;
+                return (
+                  <Link
+                    key={key}
+                    to={gerarLinkRelatorio(
+                      pedido.alunos_cei_e_ou_emei
+                        ? ALTERACAO_TIPO_ALIMENTACAO_CEMEI
+                        : ALTERACAO_TIPO_ALIMENTACAO,
+                      pedido,
+                    )}
+                  >
+                    <tr className="row">
+                      <td className="col-3">{pedido.id_externo}</td>
+                      <td className="col-3">{pedido.escola.codigo_eol}</td>
+                      <td className="col-3">{pedido.escola.nome}</td>
+                      <td className="col-3">
+                        {pedido.data_inicial ||
+                          pedido.alterar_dia ||
+                          dataMaisProxima ||
+                          pedido.data}
+                      </td>
+                    </tr>
+                  </Link>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </Collapse>
+    </div>
+  );
+};
